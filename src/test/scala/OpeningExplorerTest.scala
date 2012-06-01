b4ade16be26c47472cec819f49a45c9f844afe7e
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-01 20:27:04 +0200

    Test failing opening detection

--- src/test/scala/OpeningExplorerTest.scala
+++ src/test/scala/OpeningExplorerTest.scala
@@ -4,6 +4,8 @@ class OpeningExplorerTest extends ChessTest {
 
   def name(g: String) = OpeningExplorer openingOf g map (_.name)
 
+  def code(g: String) = OpeningExplorer openingOf g map (_.code)
+
   val g1 = "d4 Nf6 e4 Nxe4 f3 Nd6 g3" 
   g1 in {
     name(g1) must_== Some("Indian Defense")
@@ -23,5 +25,17 @@ class OpeningExplorerTest extends ChessTest {
   val g5 = "e4 e5 b3 Nc6 Nc3"
   g5 in {
     name(g5) must_== Some("King's Pawn Game")
+  }
+  val g6 = "e4 Nf6 e5 Nd5 d4"
+  g6 in {
+    code(g6) must_== Some("B03")
+  }
+  val g7 = "e4 Nf6 e5 Nd5 d4 d6 Nf3"
+  g7 in {
+    code(g7) must_== Some("B04")
+  }
+  val g8 = "e4 Nf6 e5 Nd5 d4 d6 a3"
+  g8 in {
+    code(g8) must_== Some("B03")
   }
 }

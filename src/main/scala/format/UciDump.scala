bcfc1818eee8578e6a2d2b546b13aa64ab77f620
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-24 17:02:54 +0200

    uci dump empty pgn - 1.18

--- src/main/scala/format/UciDump.scala
+++ src/main/scala/format/UciDump.scala
@@ -5,10 +5,12 @@ object UciDump {
 
   // a2a4, b8c6
   def apply(pgn: String, initialFen: Option[String]): Valid[String] =
-    PgnReader( 
-      pgn,
-      initialFen.fold(Fen(_) :: Nil, Nil)
-    ) map (_.chronoMoves map move mkString " ")
+    pgn.trim.some filter ("" !=) fold (
+      nonEmptyPgn ⇒ PgnReader(
+        nonEmptyPgn,
+        initialFen.fold(Fen(_) :: Nil, Nil)
+      ) map (_.chronoMoves map move mkString " "),
+      success(""))
 
   private def move(m: Move): String =
     m.orig.key + m.dest.key + m.promotion.fold(_.pgn, "")
--- src/test/scala/format/UciDumpTest.scala
+++ src/test/scala/format/UciDumpTest.scala
@@ -8,6 +8,11 @@ class UciDumpTest extends ChessTest {
   import PgnFixtures._
 
   "only raw moves" should {
+    "empty" in {
+      UciDump("", None) must beSuccess.like {
+        case moves ⇒ moves must_== ""
+      }
+    }
     "simple" in {
       UciDump(simple, None) must beSuccess.like {
         case moves ⇒ moves must_== "e2e3 b8c6 d2d4 g8f6"

7286afcdc9cd3db9741d87f4fc67f7796c331618
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-24 13:43:31 +0200

    chess960 support for UciDump - bump version 1.16

--- src/main/scala/format/UciDump.scala
+++ src/main/scala/format/UciDump.scala
@@ -4,8 +4,11 @@ package format
 object UciDump {
 
   // a2a4, b8c6
-  def apply(pgn: String): Valid[String] = 
-    PgnReader(pgn) map (_.chronoMoves map move mkString " ")
+  def apply(pgn: String, initialFen: Option[String]): Valid[String] =
+    PgnReader( 
+      pgn,
+      initialFen.fold(Fen(_) :: Nil, Nil)
+    ) map (_.chronoMoves map move mkString " ")
 
   private def move(m: Move): String =
     m.orig.key + m.dest.key + m.promotion.fold(_.pgn, "")

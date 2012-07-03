f0413fd418a77c72cd2960ea45e69d20d76882d0
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-07-03 23:34:20 +0200

    add NAG symbols

--- src/main/scala/format/Nag.scala
+++ src/main/scala/format/Nag.scala
@@ -1,9 +1,13 @@
 package chess.format
 
-sealed abstract class Nag(val code: Int)
+// http://en.wikipedia.org/wiki/Numeric_Annotation_Glyphs
+sealed abstract class Nag(
+  val code: Int,
+  val symbol: String
+)
 
 object Nag {
-  case object Blunder extends Nag(4)
-  case object Mistake extends Nag(2)
-  case object Inaccuracy extends Nag(6)
+  case object Blunder extends Nag(4, "??")
+  case object Mistake extends Nag(2, "?")
+  case object Inaccuracy extends Nag(6, "?!")
 }

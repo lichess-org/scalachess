f4ba2e80afcc5a7c2b596584bd7d838a7964b071
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-07-25 17:25:47 +0200

    add joda time to the dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -8,18 +8,20 @@ trait Resolvers {
 trait Dependencies {
   val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.4"
   val specs2 = "org.specs2" %% "specs2" % "1.11"
-  val scalalib = "com.github.ornicar" %% "scalalib" % "1.36"
+  val scalalib = "com.github.ornicar" %% "scalalib" % "1.42"
   val hasher = "com.roundeights" % "hasher" % "0.3" from "http://cloud.github.com/downloads/Nycto/Hasher/hasher_2.9.1-0.3.jar"
+  val jodaTime = "joda-time" % "joda-time" % "2.1"
+  val jodaConvert = "org.joda" % "joda-convert" % "1.2"
 }
 
 object ApplicationBuild extends Build with Resolvers with Dependencies {
 
   private val buildSettings = Project.defaultSettings ++ Seq(
     organization := "com.github.ornicar",
-    version := "2.10",
+    version := "2.11",
     scalaVersion := "2.9.1",
     resolvers := Seq(iliaz),
-    libraryDependencies := Seq(scalaz, scalalib, hasher),
+    libraryDependencies := Seq(scalaz, scalalib, hasher, jodaTime, jodaConvert),
     libraryDependencies in test := Seq(specs2),
     shellPrompt := {
       (state: State) ⇒ "%s> ".format(Project.extract(state).currentProject.id)

438ccf05f65963cb07c253231ac3487ac3430a81
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-11-29 15:46:15 +0100

    upgrade to scala 2.10

--- project/Build.scala
+++ project/Build.scala
@@ -6,9 +6,9 @@ trait Resolvers {
 }
 
 trait Dependencies {
-  val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.4"
-  val specs2 = "org.specs2" %% "specs2" % "1.12"
-  val scalalib = "com.github.ornicar" %% "scalalib" % "2.4"
+  val scalaz = "org.scalaz" % "scalaz-core_2.10.0-RC2" % "6.0.4"
+  val specs2 = "org.specs2" % "specs2_2.10.0-RC2" % "1.12.2"
+  val scalalib = "com.github.ornicar" % "scalalib_2.9.1" % "2.5"
   val hasher = "com.roundeights" % "hasher" % "0.3" from "http://cloud.github.com/downloads/Nycto/Hasher/hasher_2.9.1-0.3.jar"
   val jodaTime = "joda-time" % "joda-time" % "2.1"
   val jodaConvert = "org.joda" % "joda-convert" % "1.2"
@@ -18,8 +18,7 @@ object ApplicationBuild extends Build with Resolvers with Dependencies {
 
   private val buildSettings = Project.defaultSettings ++ Seq(
     organization := "com.github.ornicar",
-    version := "2.14",
-    scalaVersion := "2.9.1",
+    version := "3",
     resolvers := Seq(iliaz),
     libraryDependencies := Seq(scalaz, scalalib, hasher, jodaTime, jodaConvert),
     libraryDependencies in test := Seq(specs2),

1b6a57642983ac488162efb2d7dc223530fe3085
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-09 21:48:51 -0300

    Accept empty PGN for UCI dump

--- src/main/scala/format/UciDump.scala
+++ src/main/scala/format/UciDump.scala
@@ -6,15 +6,15 @@ import pgn.{ Reader, Tag }
 object UciDump {
 
   // a2a4, b8c6
-  def apply(pgn: String, initialFen: Option[String]): Valid[String] = for {
-    nonEmptyPgn ← pgn.trim.some filter ("" !=) toValid "Empty pgn"
-    moves ← Reader(
-      nonEmptyPgn,
-      initialFen.fold(List[Tag]()) { fen ⇒
-        Tag(_.FEN, fen) :: Tag(_.Variant, chess.Variant.Chess960.name) :: Nil
-      }
-    ) map (_.chronoMoves map move mkString " ")
-  } yield moves
+  def apply(pgn: String, initialFen: Option[String]): Valid[String] =
+    pgn.trim.some.filter("" !=).fold[Valid[String]](success("")) { nonEmptyPgn ⇒
+      Reader(
+        nonEmptyPgn,
+        initialFen.fold(List[Tag]()) { fen ⇒
+          Tag(_.FEN, fen) :: Tag(_.Variant, chess.Variant.Chess960.name) :: Nil
+        }
+      ) map { _.chronoMoves map move mkString " " }
+    }
 
   private def move(m: Move): String =
     m.orig.key + m.dest.key + m.promotion.fold("")(_.forsyth.toString)

a1e00c5962d30c6062368d08dbd19b637f41a779
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-08 23:37:37 +0200

    explicitly provide variant to UciDump

--- src/main/scala/format/UciDump.scala
+++ src/main/scala/format/UciDump.scala
@@ -6,20 +6,23 @@ import pgn.{ Reader, Tag }
 object UciDump {
 
   // a2a4, b8c6
-  def apply(pgn: String, initialFen: Option[String]): Valid[String] =
-    pgn.trim.some.filter("" !=).fold[Valid[String]](success("")) { nonEmptyPgn ⇒
+  def apply(pgn: String, initialFen: Option[String], variant: Option[Variant]): Valid[String] =
+    pgn.trim.some.filter(_.nonEmpty).fold[Valid[String]](success("")) { nonEmptyPgn ⇒
       Reader(
         nonEmptyPgn,
-        initialFen.fold(List[Tag]()) { fen ⇒
-          Tag(_.FEN, fen) :: Tag(_.Variant, chess.Variant.Chess960.name) :: Nil
+        List(
+          (initialFen map { fen ⇒ Tag(_.FEN, fen) }),
+          (variant map { v ⇒ Tag(_.Variant, v.name) })
+        ).flatten
+      ) map {
+          _.chronoMoves map { m ⇒
+            m.castle.fold(
+              m.orig.key + m.dest.key + m.promotion.fold("")(_.forsyth.toString)
+            ) {
+                case ((kf, kt), (rf, rt)) if kf == kt || variant == Some(Variant.Chess960) ⇒ kf.key + rf.key
+                case ((kf, kt), _) ⇒ kf.key + kt.key
+              }
+          } mkString " "
         }
-      ) map { _.chronoMoves map move mkString " " }
-    }
-
-  private def move(m: Move): String = m.castle.fold(
-    m.orig.key + m.dest.key + m.promotion.fold("")(_.forsyth.toString)
-  ) {
-      case ((kf, kt), (rf, rt)) if kf == kt ⇒ kf.key + rf.key
-      case ((kf, kt), _) ⇒ kf.key + kt.key
     }
 }

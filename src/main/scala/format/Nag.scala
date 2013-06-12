ead8ecf3ea478e69135bf2ac95a9d773fb7048ec
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-12 13:53:52 +0200

    add Nag list

--- src/main/scala/format/Nag.scala
+++ src/main/scala/format/Nag.scala
@@ -10,4 +10,6 @@ object Nag {
   case object Blunder extends Nag(4, "??")
   case object Mistake extends Nag(2, "?")
   case object Inaccuracy extends Nag(6, "?!")
+
+  val all = List(Blunder, Mistake, Inaccuracy)
 }

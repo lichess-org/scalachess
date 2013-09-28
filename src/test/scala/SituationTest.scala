afa5f274d84b04c8ef6d666a14285baaef19b92a
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-28 11:31:22 +0200

    more check tests

--- src/test/scala/SituationTest.scala
+++ src/test/scala/SituationTest.scala
@@ -3,59 +3,71 @@ package chess
 class SituationTest extends ChessTest {
 
   "a game" should {
-    "detect" in {
-      "check" in {
-        "by rook" in {
-          ("""
+    "detect check" should {
+      "by rook" in {
+        ("""
 K  r
 """ as White).check must beTrue
-        }
-        "by knight" in {
-          ("""
+      }
+      "by knight" in {
+        ("""
   n
 K
 """ as White).check must beTrue
-        }
-        "not" in {
-          ("""
+      }
+      "by bishop" in {
+        ("""
+  b
+
+   
+     K
+""" as White).check must beTrue
+      }
+      "by pawn" in {
+        ("""
+    p
+     K
+""" as White).check must beTrue
+      }
+      "not" in {
+        ("""
    n
 K
 """ as White).check must beFalse
-        }
       }
-      "check mate" in {
-        "by rook" in {
-          ("""
+    }
+    "detect check mate" in {
+      "by rook" in {
+        ("""
 PP
 K  r
 """ as White).checkMate must beTrue
-        }
-        "by knight" in {
-          ("""
+      }
+      "by knight" in {
+        ("""
 PPn
 KR
 """ as White).checkMate must beTrue
-        }
-        "not" in {
-          ("""
+      }
+      "not" in {
+        ("""
   n
 K
 """ as White).checkMate must beFalse
-        }
       }
-      "stale mate" in {
-        "stuck in a corner" in {
-          ("""
+    }
+    "stale mate" in {
+      "stuck in a corner" in {
+        ("""
 prr
 K
 """ as White).staleMate must beTrue
-        }
-        "not" in {
-          ("""
+      }
+      "not" in {
+        ("""
   b
 K
 """ as White).staleMate must beFalse
-        }
       }
     }
   }

b3f0159f7ec0d5d15448470d704f41789d01942f
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-10-04 10:07:48 +0200

    tweak UciMove implementation

--- src/main/scala/format/UciMove.scala
+++ src/main/scala/format/UciMove.scala
@@ -15,12 +15,12 @@ object UciMove {
   def apply(move: String): Option[UciMove] = for {
     orig ← Pos.posAt(move take 2)
     dest ← Pos.posAt(move drop 2 take 2)
-    promotion = (move drop 4).headOption flatMap Role.promotable
+    promotion = move lift 5 flatMap Role.promotable
   } yield UciMove(orig, dest, promotion)
 
   def piotr(move: String): Option[UciMove] = for {
     orig ← move.headOption flatMap Pos.piotr
-    dest ← move.tail.headOption flatMap Pos.piotr
-    promotion = move.tail.tail.headOption flatMap Role.promotable
+    dest ← move lift 1 flatMap Pos.piotr
+    promotion = move lift 2 flatMap Role.promotable
   } yield UciMove(orig, dest, promotion)
 }

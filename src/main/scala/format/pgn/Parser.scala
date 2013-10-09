338e7687a7f14ce50f3a06bc1f3d52748c4ce1b9
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-10-09 14:55:06 +0200

    rewrite PGN parsing using 3 parser combinators

--- src/main/scala/format/pgn/Parser.scala
+++ src/main/scala/format/pgn/Parser.scala
@@ -3,104 +3,120 @@ package format.pgn
 
 import scala.util.parsing.combinator._
 
-object Parser {
+// http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm
+object Parser
+    extends scalaz.std.OptionInstances
+    with scalaz.syntax.ToTraverseOps {
 
   def apply(pgn: String): Valid[ParsedPgn] = for {
-    splitted ← splitSanAndMoves(sanitize(pgn))
+    splitted ← splitTagAndMoves(pgn)
     (tagStr, moveStr) = splitted
     tags ← TagParser(tagStr)
-    sans ← MoveParser(moveStr)
+    sanStrs ← MovesParser(moveStr)
+    sans ← sanStrs.map(MoveParser.apply).sequence
   } yield ParsedPgn(tags, sans)
 
-  private val SanitizeRegex = """(\$\d+)|(\{[^\}]+\})""".r
-  private val DoubleSpaceRegex = """\s{2,}""".r
-  private def sanitize(pgn: String): String = DoubleSpaceRegex.replaceAllIn(
-    SanitizeRegex.replaceAllIn(
-      pgn.lines.map(_.trim).filterNot(_.isEmpty) mkString "\n", 
-      ""),
-    " ")
-
-  private def splitSanAndMoves(pgn: String): Valid[(String, String)] =
-    pgn.lines.toList span { line ⇒
-      ~((line lift 0).map('[' ==))
-    } match {
-      case (tagLines, moveLines) ⇒ success(tagLines.mkString("\n") -> moveLines.mkString(" ").trim)
-    }
-
-  object TagParser extends RegexParsers {
-
-    def apply(pgn: String): Valid[List[Tag]] = parseAll(all, pgn) match {
-      case f: Failure       ⇒ "Cannot parse tags: %s\n%s".format(f.toString, pgn).failNel
-      case Success(sans, _) ⇒ scalaz.Validation.success(sans)
-      case err              ⇒ "Cannot parse tags: %s\n%s".format(err.toString, pgn).failNel
-    }
-
-    def all: Parser[List[Tag]] = tags <~ """(.|\n)*""".r 
-
-    def tags: Parser[List[Tag]] = rep(tag)
-
-    def tag: Parser[Tag] = tagName ~ tagValue ^^ {
-      case name ~ value ⇒ Tag(name, value)
-    }
-
-    val tagName: Parser[String] = "[" ~> """[a-zA-Z]+""".r
-
-    val tagValue: Parser[String] = "\"" ~> """[^"]+""".r <~ "\"]"
+  trait Logging { self: Parsers ⇒
+    protected val loggingEnabled = false
+    protected def as[T](msg: String)(p: ⇒ Parser[T]): Parser[T] =
+      if (loggingEnabled) log(p)(msg) else p
   }
 
-  object MoveParser extends RegexParsers {
+  object MovesParser extends RegexParsers with Logging {
 
-    val CRLF = "\r\n" | "\n"
-    val space = """\s+""".r
+    override val whiteSpace = """(\s|\t|\r?\n)+""".r
+    def eol = """(\r?\n)+""".r
 
-    override val whiteSpace = "".r
-
-    def apply(pgn: String): Valid[List[San]] =
-      parseAll(moves, (pgn.lines mkString " ")) match {
-        case Success(sans, _) ⇒ scalaz.Validation.success(sans)
-        case err              ⇒ "Cannot parse moves: %s\n%s".format(err.toString, pgn).failNel
+    def apply(pgn: String): Valid[List[String]] =
+      parseAll(moves, pgn) match {
+        case Success(moves, _) ⇒ scalaz.Validation.success(moves)
+        case err               ⇒ "Cannot parse moves: %s\n%s".format(err.toString, pgn).failNel
       }
 
-    def moves: Parser[List[San]] = repsep(move, space) <~ (result?)
+    def moves: Parser[List[String]] = as("moves") {
+      rep(move) <~ (result?)
+    }
 
-    val result: Parser[String] = space ~> ("*" | "1/2-1/2" | "0-1" | "1-0")
+    val moveRegex = """(0\-0|0\-0\-0|[QKRBNOoa-h][QKRBNa-h1-8xOo\-=\+\#]{1,6})""".r
 
-    def move: Parser[San] =
-      (number?) ~> (castle | standard) <~ (nag?) <~ rep(line)
+    def move: Parser[String] = as("move") {
+      ((number | commentary)*) ~> moveRegex <~ (moveExtras*)
+    }
 
-    def nag: Parser[String] = """[^\(\)\{\}]*""".r
+    def number: Parser[String] = """[1-9]\d*\s*\.*""".r
 
-    def line: Parser[List[San]] = 
-      (space?) ~> "(" ~> (space?) ~> moves <~ (space?) <~ ")"
+    def moveExtras: Parser[Unit] = as("moveExtras") {
+      (annotation | nag | variation | commentary) ^^^ ()
+    }
+
+    def annotation: Parser[String] =
+      "!" | "?" | "!!" | "!?" | "?!" | "??" | "□"
+
+    def nag: Parser[String] = """\$\d+""".r
+
+    def variation: Parser[List[String]] = as("variation") {
+      "(" ~> moves <~ ")"
+    }
+
+    def commentary: Parser[String] = blockCommentary | inlineCommentary
+
+    def blockCommentary: Parser[String] = as("block comment") {
+      "{" ~> """[^\}]+""".r <~ "}"
+  }
+
+    def inlineCommentary: Parser[String] = as("inline comment") {
+      ";" ~> """.+""".r 
+    }
+
+    val result: Parser[String] = "*" | "1/2-1/2" | "0-1" | "1-0"
+  }
+
+  object MoveParser extends RegexParsers with Logging {
+
+    override def skipWhitespace = false
+
+    def apply(str: String): Valid[San] =
+      parseAll(move, str) match {
+        case Success(san, _) ⇒ scalaz.Validation.success(san)
+        case err             ⇒ "Cannot parse move: %s\n%s".format(err.toString, str).failNel
+      }
+
+    def move: Parser[San] = castle | standard
 
     def castle = (qCastle | kCastle) ~ suffixes ^^ {
       case side ~ suf ⇒ Castle(side) withSuffixes suf
     }
 
-    val qCastle: Parser[Side] = "O-O-O" ^^^ QueenSide
+    val qCastle: Parser[Side] = ("O-O-O" | "o-o-o" | "0-0-0") ^^^ QueenSide
 
-    val kCastle: Parser[Side] = "O-O" ^^^ KingSide
+    val kCastle: Parser[Side] = ("O-O" | "o-o" | "0-0") ^^^ KingSide
 
-    def standard: Parser[Std] = (complete | simple | disambiguated) ~ suffixes ^^ {
-      case std ~ suf ⇒ std withSuffixes suf
+    def standard: Parser[Std] = as("standard") {
+      (pawn | disambiguated | ambiguous) ~ suffixes ^^ {
+        case std ~ suf ⇒ std withSuffixes suf
+      }
     }
 
-    val number: Parser[String] = """\d+\.+""".r <~ maybeSpace
-
-    val maybeSpace = space?
-
-    def simple: Parser[Std] = role ~ x ~ dest ^^ {
-      case ro ~ ca ~ de ⇒ Std(dest = de, role = ro, capture = ca)
+    // e5
+    def pawn: Parser[Std] = as("pawn") {
+      dest ^^ {
+        case de ⇒ Std(dest = de, role = Pawn)
+      }
     }
 
-    def disambiguated: Parser[Std] = role ~ opt(file) ~ opt(rank) ~ x ~ dest ^^ {
-      case ro ~ fi ~ ra ~ ca ~ de ⇒ Std(
-        dest = de, role = ro, capture = ca, file = fi, rank = ra)
+    // Bg5
+    def ambiguous: Parser[Std] = as("ambiguous") {
+      role ~ x ~ dest ^^ {
+        case ro ~ ca ~ de ⇒ Std(dest = de, role = ro, capture = ca)
+      }
     }
 
-    def complete: Parser[Std] = role ~ file ~ rank ~ x ~ dest ^^ {
-      case ro ~ fi ~ ra ~ ca ~ de ⇒ Std(
-        dest = de, role = ro, capture = ca, file = fi.some, rank = ra.some)
+    // Bac3 Baxc3 B2c3 B2xc3 Ba2xc3
+    def disambiguated: Parser[Std] = as("disambiguated") {
+      role ~ opt(file) ~ opt(rank) ~ x ~ dest ^^ {
+        case ro ~ fi ~ ra ~ ca ~ de ⇒ Std(
+          dest = de, role = ro, capture = ca, file = fi, rank = ra)
+      }
     }
 
     def suffixes: Parser[Suffixes] = opt(promotion) ~ check ~ checkmate ^^ {
@@ -134,4 +150,36 @@ object Parser {
         case (acc, (a, b)) ⇒ a.toString ^^^ b | acc
       }
   }
+
+  private object TagParser extends RegexParsers with Logging {
+
+    def apply(pgn: String): Valid[List[Tag]] = parseAll(all, pgn) match {
+      case f: Failure       ⇒ "Cannot parse tags: %s\n%s".format(f.toString, pgn).failNel
+      case Success(sans, _) ⇒ scalaz.Validation.success(sans)
+      case err              ⇒ "Cannot parse tags: %s\n%s".format(err.toString, pgn).failNel
+    }
+
+    def all: Parser[List[Tag]] = as("all") {
+      tags <~ """(.|\n)*""".r
+    }
+
+    def tags: Parser[List[Tag]] = rep(tag)
+
+    def tag: Parser[Tag] = as("tag") {
+      tagName ~ tagValue ^^ {
+        case name ~ value ⇒ Tag(name, value)
+      }
+    }
+
+    val tagName: Parser[String] = "[" ~> """[a-zA-Z]+""".r
+
+    val tagValue: Parser[String] = "\"" ~> """[^"]+""".r <~ "\"]"
+  }
+
+  private def splitTagAndMoves(pgn: String): Valid[(String, String)] =
+    pgn.lines.toList.map(_.trim).filter(_.nonEmpty) span { line ⇒
+      ~((line lift 0).map('[' ==))
+    } match {
+      case (tagLines, moveLines) ⇒ success(tagLines.mkString("\n") -> moveLines.mkString("\n"))
+    }
 }

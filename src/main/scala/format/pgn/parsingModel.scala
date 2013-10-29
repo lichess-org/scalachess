e3462cf9f8b330a0586070706a52c9dec6b02e99
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-10-29 12:38:36 +0100

    fix pgn parsing error messages

--- src/main/scala/format/pgn/parsingModel.scala
+++ src/main/scala/format/pgn/parsingModel.scala
@@ -33,9 +33,9 @@ case class Std(
     } collect {
       case Some(m) if compare(file, m.orig.x) && compare(rank, m.orig.y) ⇒ m
     } match {
-      case Nil        ⇒ s"${game.id} No move found: $this\n${game.board}".failNel
+      case Nil        ⇒ s"No move found: $this\n${game.board}".failNel
       case one :: Nil ⇒ one withPromotion promotion toValid "Wrong promotion"
-      case many       ⇒ s"${game.id} Many moves found: $many\n${game.board}".failNel
+      case many       ⇒ s"Many moves found: $many\n${game.board}".failNel
     }
   }
 

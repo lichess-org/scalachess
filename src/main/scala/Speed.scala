49ed2315372c230a938b7120272dd5de65ee5735
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-04-20 21:40:10 +0200

    add Speed.isUnlimited convenience function

--- src/main/scala/Speed.scala
+++ src/main/scala/Speed.scala
@@ -1,19 +1,19 @@
 package chess
 
 sealed abstract class Speed(
-  val id: Int, 
-  val range: Range,
-  val name: String) {
+    val id: Int,
+    val range: Range,
+    val name: String) {
 
-    def shortName = toString.toLowerCase
+  def shortName = toString.toLowerCase
 }
 
 object Speed {
 
-  case object Bullet extends Speed(1, 0 to 179, "Less than 3 minutes") 
-  case object Blitz extends Speed(2, 180 to 479, "3 to 8 minutes") 
-  case object Slow extends Speed(3, 480 to 1199, "8 to 20 minutes") 
-  case object Unlimited extends Speed(4, 1200 to Int.MaxValue, "No clock") 
+  case object Bullet extends Speed(1, 0 to 179, "Less than 3 minutes")
+  case object Blitz extends Speed(2, 180 to 479, "3 to 8 minutes")
+  case object Slow extends Speed(3, 480 to 1199, "8 to 20 minutes")
+  case object Unlimited extends Speed(4, 1200 to Int.MaxValue, "No clock")
 
   val all = List(Bullet, Blitz, Slow, Unlimited)
   val byId = all map { v => (v.id, v) } toMap
@@ -22,6 +22,8 @@ object Speed {
   def apply(id: Int): Option[Speed] = byId get id
 
   def apply(clock: Option[Clock]) = byTime(clock.fold(Int.MaxValue)(_.estimateTotalTime))
+
+  def isUnlimited(clock: Clock) = byTime(clock.estimateTotalTime) == Unlimited
 
   def byTime(seconds: Int): Speed = all.find(_.range contains seconds) | Unlimited
 

6062233f802c61ba430b974a63d970208a8e2862
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-05-13 22:51:36 +0200

    fix opening tests

--- src/test/scala/OpeningExplorerTest.scala
+++ src/test/scala/OpeningExplorerTest.scala
@@ -6,29 +6,29 @@ class OpeningExplorerTest extends ChessTest {
 
   def code(g: String) = OpeningExplorer openingOf g.split(' ').toList map (_.code)
 
-  val g1 = "d4 Nf6 e4 Nxe4 f3 Nd6 g3" 
+  val g1 = "d4 Nf6 e4 Nxe4 f3 Nd6 g3"
   g1 in {
-    name(g1) must_== Some("Indian Defense")
+    name(g1) must_== Some("Indian Game, General")
   }
-  val g2 = "e4 e5 Nf3 Nf6" 
+  val g2 = "e4 e5 Nf3 Nf6"
   g2 in {
-    name(g2) must_== Some("Petrov Defense")
+    name(g2) must_== Some("Russian Game, General")
   }
-  val g3 = "e4 e5" 
+  val g3 = "e4 e5"
   g3 in {
-    name(g3) must_== Some("King's Pawn Game")
+    name(g3) must_== Some("Sicilian Defense, General")
   }
-  val g4 = "e4 e5 b3 Nc6" 
+  val g4 = "e4 e5 b3 Nc6"
   g4 in {
-    name(g4) must_== Some("King's Pawn Game")
+    name(g4) must_== Some("Sicilian Defense, General")
   }
   val g5 = "e4 e5 b3 Nc6 Nc3"
   g5 in {
-    name(g5) must_== Some("King's Pawn Game")
+    name(g5) must_== Some("Sicilian Defense, General")
   }
   val g6 = "e4 Nf6 e5 Nd5 d4"
   g6 in {
-    code(g6) must_== Some("B03")
+    code(g6) must_== Some("B02")
   }
   val g7 = "e4 Nf6 e5 Nd5 d4 d6 Nf3"
   g7 in {

1ceba9320d94c506b513b735d8c6f8852563a591
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-07-28 01:04:05 +0200

    s/Slow/Classical

--- src/main/scala/Speed.scala
+++ src/main/scala/Speed.scala
@@ -10,10 +10,10 @@ object Speed {
 
   case object Bullet extends Speed(1, 0 to 179, "Bullet", "Less than 3 minutes")
   case object Blitz extends Speed(2, 180 to 479, "Blitz", "3 to 8 minutes")
-  case object Slow extends Speed(3, 480 to 1199, "Classical", "8 to 20 minutes")
+  case object Classical extends Speed(3, 480 to 1199, "Classical", "8 to 20 minutes")
   case object Unlimited extends Speed(4, 1200 to Int.MaxValue, "Unlimited", "No clock")
 
-  val all = List(Bullet, Blitz, Slow, Unlimited)
+  val all = List(Bullet, Blitz, Classical, Unlimited)
   val byId = all map { v => (v.id, v) } toMap
   val byName = all map { v => (v.name, v) } toMap
 

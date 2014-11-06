b968c411f39220e868966b9195f7e01e1391fc1b
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-06 09:17:18 +0100

    remove testing of dead pieces, which are gone

--- src/test/scala/GameTest.scala
+++ src/test/scala/GameTest.scala
@@ -5,29 +5,6 @@ import Pos._
 
 class GameTest extends ChessTest {
 
-  "capture a piece" should {
-    "add it to the dead pieces" in {
-      val game = makeGame.playMoves(
-        E2 -> E4,
-        D7 -> D5,
-        E4 -> D5)
-      game must beSuccess.like {
-        case g => g.deads must containTheSameElementsAs(List(Black.pawn))
-      }
-    }
-  }
-  "recapture a piece" should {
-    "add both to the dead pieces" in {
-      val game = Game("""
-bq
-R""").playMoves(
-        A1 -> A2,
-        B2 -> A2)
-      game must beSuccess.like {
-        case g => g.deads must containTheSameElementsAs(List(Black.bishop, White.rook))
-      }
-    }
-  }
   "prevent castle by capturing a rook" should {
     val game = Game("""
  b

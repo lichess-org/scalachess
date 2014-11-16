a48295b37e703c102d46a06badfcb3fc34c09837
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-16 14:47:51 +0100

    Divider coding standards and performance testing

--- src/main/scala/Divider.scala
+++ src/main/scala/Divider.scala
@@ -5,59 +5,58 @@ case class Division(mid: Option[Int], end: Option[Int])
 object Divider {
 
   def apply(replay: Replay): Division = {
+
     val boards = replay.chronoMoves.map { _.before }
 
-    val midGame = boards.toStream.map( i => 
-      (mixedness(i), majorsAndMinors(i), backRankSparse(i)) 
-    ).indexWhere( i => i._1 > 150 || i._2 <= 10 || i._3 == true )
+    val midGame = boards.toStream.map(i =>
+      (mixedness(i), majorsAndMinors(i), backRankSparse(i))
+    ).indexWhere(i => i._1 > 150 || i._2 <= 10 || i._3 == true)
 
-    val endGame = boards.toStream.map(majorsAndMinors).indexWhere( _ <= 6)
-      
+    val endGame = boards.toStream.map(majorsAndMinors).indexWhere(_ <= 6)
+
     Division(
-      if (midGame < endGame || endGame == -1) indexOption(midGame) else None
-    ,
+      if (midGame < endGame || endGame == -1) indexOption(midGame) else None,
       indexOption(endGame)
     )
-
   }
 
-  def majorsAndMinors(board: Board): Int = board.pieces.values.map {
+  private def majorsAndMinors(board: Board): Int = board.pieces.values.map {
     _.role match {
-      case Queen => 1
+      case Queen  => 1
       case Bishop => 1
       case Knight => 1
-      case Rook => 1
-      case _ => 0
+      case Rook   => 1
+      case _      => 0
     }
   }.sum
 
-  def backRankSparse(board: Board): Boolean = {
+  private def backRankSparse(board: Board): Boolean = {
     // Sparse back-rank indicates that pieces have been developed
-    val white = (1 to 8).flatMap( x => 
-      board(x, 1).map( piece =>
+    val white = (1 to 8).flatMap(x =>
+      board(x, 1).map(piece =>
         if (piece is Color.white) 1 else 0
       )
     ).sum
 
-    val black = (1 to 8).flatMap( x => 
-      board(x, 8).map( piece =>
+    val black = (1 to 8).flatMap(x =>
+      board(x, 8).map(piece =>
         if (piece is Color.black) 1 else 0
       )
     ).sum
 
-    if (black <= 3 || white <= 3 ) true else false
+    (black <= 3 || white <= 3)
   }
 
-  def score(white: Int, black: Int, x: Int, y: Int): Int = (white, black) match {
+  private def score(white: Int, black: Int, x: Int, y: Int): Int = (white, black) match {
     case (0, 0) => 0
 
-    case (1, 0) => 1 + (8-y)
+    case (1, 0) => 1 + (8 - y)
     case (2, 0) => if (y > 2) 2 + (y - 2) else 0
     case (3, 0) => if (y > 1) 3 + (y - 1) else 0
     case (4, 0) => if (y > 1) 3 + (y - 1) else 0 // group of 4 on the homerow = 0
 
     case (0, 1) => 1 + y
-    case (1, 1) => 5 + (3-y).abs
+    case (1, 1) => 5 + (3 - y).abs
     case (2, 1) => 4 + y
     case (3, 1) => 5 + y
 
@@ -70,21 +69,21 @@ object Divider {
 
     case (0, 4) => if (y < 7) 3 + (7 - y) else 0
 
-    case _ => 0
+    case _      => 0
   }
 
-  def mixedness(board: Board): Int = {
-    (1 to 7).flatMap( y =>
-      (1 to 7).map( x =>
+  private def mixedness(board: Board): Int = {
+    (1 to 7).flatMap(y =>
+      (1 to 7).map(x =>
         {
-          val cell = (0 to 1).flatMap( yp =>
-            (0 to 1).map( xp =>
+          val cell = (0 to 1).flatMap(yp =>
+            (0 to 1).map(xp =>
               board(x + xp, y + yp).map(
                 piece =>
                   if (piece is Color.white) 1 else -1
               ).sum
             )
-          ).groupBy( i => i ).mapValues(_.size)
+          ).groupBy(i => i).mapValues(_.size)
 
           score(cell.getOrElse(1, 0), cell.getOrElse(-1, 0), x, y)
         }
@@ -92,5 +91,5 @@ object Divider {
     ).sum
   }
 
-  def indexOption(index: Int) = if (index == -1) None else Some(index)
+  private def indexOption(index: Int) = if (index == -1) None else Some(index)
 }
---
+++ src/test/scala/DividerPerfTest.scala
@@ -0,0 +1,40 @@
+package chess
+
+import Pos._
+
+class DividerPerfTest extends ChessTest {
+
+  val nb = 100
+  val iterations = 10
+  // val nb = 1
+  // val iterations = 1
+
+  val gameReplay = format.pgn.Reader.full("1. e4 c5 2. Nf3 d6 3. Bc4 Nf6 4. d3 g6 5. c3 Bg7 6. Bg5 O-O 7. h3 Nc6 8. Nbd2 a6 9. Bb3 b5 10. Bc2 Bb7 11. O-O Nd7 12. Nh2 f6 13. Be3 e5 14. Ndf3 Ne7 15. Qd2 f5 16. Qe2 h6 17. Bd2 g5 18. g4 f4 19. Bb3+ d5 20. exd5 Bxd5 21. c4 Bf7 22.  cxb5 axb5 23. Bxf7+ Rxf7 24. Bc3 Ng6 25. a3 b4 26. axb4 Rxa1 27. Rxa1 cxb4 28. Bxb4 Qb6 29. Bc3 Re7 30. Ra8+ Kh7 31. Kg2 Nc5 32. Qc2 Qb3 33. Qxb3 Nxb3 34. Rb8 Nc5 35. Rb5 Nxd3 36. Nf1 e4 37. Ng1 Nh4+ 38. Kh2 Bxc3 39. bxc3 Nxf2 40. Rd5 e3 41. Ne2 Nf3+ 42. Kg2 Ne1+ 43. Kh2 f3 44. Neg3 e2 45. Nd2 Ng2 46. Nxf3 e1=Q 47. Nxe1 Nxe1 48. c4 Nc2 49. c5 Ne4 50. c6 Nxg3 51. Kxg3 Re3+ 52. Kf2 Rc3 53. Rd7+ Kg6 54. c7 Nb4 55. Rd6+ Kf7 56. Rxh6 Rxc7 57. Rh7+ Ke6 58. Rxc7 Nd3+ 59. Kf3 Nf4 60. Kg3 Ne2+ 61. Kf2 Nf4 62. Kg3 Ne2+ 63. Kh2 Nf4 64. Rc5 Kf6 65.  Rf5+ Kg6 66. Kg3 Ne2+ 67. Kf3 Nd4+ 68. Ke4 Nxf5 69. gxf5+ Kf6 70. h4 gxh4 71. Kf4 h3 72. Kg3 Kxf5 73. Kxh3 1/2-1/2").err
+  def runOne = Divider(gameReplay)
+  def run { for (i ← 1 to nb) runOne }
+
+  "playing a game" should {
+    "many times" in {
+      runOne.end must beSome.like {
+        case x => x must beBetween(65, 80)
+      }
+      if (nb * iterations > 1) {
+        println("warming up")
+        run
+      }
+      println("running tests")
+      val durations = for (i ← 1 to iterations) yield {
+        val start = System.currentTimeMillis
+        run
+        val duration = System.currentTimeMillis - start
+        println(s"$nb games in $duration ms")
+        duration
+      }
+      val nbGames = iterations * nb
+      val moveMicros = (1000 * durations.sum) / nbGames
+      println(s"Average = $moveMicros microseconds per game")
+      println(s"          ${1000000 / moveMicros} games per second")
+      true === true
+    }
+  }
+}

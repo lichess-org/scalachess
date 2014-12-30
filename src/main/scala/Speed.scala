0cefcae7c8ed7a9f26b07d109c398079178a35ce
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-12-30 16:52:04 +0100

    rename unlimited speed as correspondence

--- src/main/scala/Speed.scala
+++ src/main/scala/Speed.scala
@@ -14,9 +14,9 @@ object Speed {
   case object Bullet extends Speed(1, 0 to 179, "Bullet", "Very fast games: less than 3 minutes")
   case object Blitz extends Speed(2, 180 to 479, "Blitz", "Fast games: 3 to 8 minutes")
   case object Classical extends Speed(3, 480 to 21599, "Classical", "Classical games: more than 8 minutes")
-  case object Unlimited extends Speed(4, 21600 to Int.MaxValue, "Unlimited", "Games without a Chess clock")
+  case object Correspondence extends Speed(4, 21600 to Int.MaxValue, "Correspondence", "Correspondence games")
 
-  val all = List(Bullet, Blitz, Classical, Unlimited)
+  val all = List(Bullet, Blitz, Classical, Correspondence)
   val limited = List(Bullet, Blitz, Classical)
 
   val byId = all map { v => (v.id, v) } toMap
@@ -25,9 +25,7 @@ object Speed {
 
   def apply(clock: Option[Clock]) = byTime(clock.fold(Int.MaxValue)(_.estimateTotalTime))
 
-  def isUnlimited(clock: Clock) = byTime(clock.estimateTotalTime) == Unlimited
-
-  def byTime(seconds: Int): Speed = all.find(_.range contains seconds) | Unlimited
+  def byTime(seconds: Int): Speed = all.find(_.range contains seconds) | Correspondence
 
   def exists(id: Int): Boolean = byId contains id
 }

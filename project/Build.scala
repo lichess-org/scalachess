cae31ef6d4c97a0c63b9c0c2b30069c54ed8fe10
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-17 12:28:29 +0100

    upgrade stuff

--- project/Build.scala
+++ project/Build.scala
@@ -1,17 +1,15 @@
 import sbt._, Keys._
 
 trait Resolvers {
-  val iliaz = "iliaz.com" at "http://scala.iliaz.com/"
   val sonatype = "sonatype" at "http://oss.sonatype.org/content/repositories/releases"
-  val awesomepom = "awesomepom" at "https://raw.github.com/jibs/maven-repo-scala/master"
-  val roundeights = "RoundEights" at "http://maven.spikemark.net/roundeights"
+  val bintray = "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
 }
 
 trait Dependencies {
-  val scalaz = "org.scalaz" %% "scalaz-core" % "7.1.0"
-  val specs2 = "org.specs2" %% "specs2" % "2.3.12" % "test"
-  val scalalib = "com.github.ornicar" %% "scalalib" % "5.2"
-  val jodaTime = "joda-time" % "joda-time" % "2.6"
+  val scalaz = "org.scalaz" %% "scalaz-core" % "7.1.1"
+  val specs2 = "org.specs2" %% "specs2-core" % "3.0.1" % "test"
+  val scalalib = "com.github.ornicar" %% "scalalib" % "5.3"
+  val jodaTime = "joda-time" % "joda-time" % "2.7"
 }
 
 object ApplicationBuild extends Build with Resolvers with Dependencies {
@@ -20,7 +18,7 @@ object ApplicationBuild extends Build with Resolvers with Dependencies {
     scalaVersion := "2.11.6",
     organization := "org.lichess",
     version := "4.1",
-    resolvers := Seq(iliaz, sonatype, awesomepom),
+    resolvers := Seq(bintray),
     libraryDependencies := Seq(scalaz, scalalib, jodaTime, specs2),
     updateOptions := updateOptions.value.withCachedResolution(true),
     scalacOptions := Seq(

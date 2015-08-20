bdac8f4acd581cc1a8aa9086903ab7c93530a4c5
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-08-20 19:54:02 +0200

    only use chess960 castling notation on chess960 games

--- src/main/scala/format/UciDump.scala
+++ src/main/scala/format/UciDump.scala
@@ -18,7 +18,7 @@ object UciDump {
   def move(variant: Variant)(m: Move): String = m.castle.fold(
     m.orig.key + m.dest.key + m.promotion.fold("")(_.forsyth.toString)
   ) {
-      case ((kf, kt), (rf, rt)) if kf == kt || !variant.standardInitialPosition => kf.key + rf.key
+      case ((kf, kt), (rf, rt)) if kf == kt || variant.chess960 => kf.key + rf.key
       case ((kf, kt), _) => kf.key + kt.key
     }
 }

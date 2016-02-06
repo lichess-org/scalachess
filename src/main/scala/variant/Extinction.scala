cd42c8efec2c7999b9169bb6559e52946e75045a
stevenhao <stevenhao@gmail.com>
2016-02-06 10:26:33 -0500

    add extinction chess

---
+++ src/main/scala/variant/Extinction.scala
@@ -0,0 +1,31 @@
+package chess
+package variant
+
+import chess.Role._
+
+case object Extinction extends Variant(
+  id = 11,
+  key = "extinct",
+  name = "Extinction",
+  shortName = "extinct",
+  title = "Capture a piece to extinction to win the game.",
+  standardInitialPosition = true) {
+
+  override def specialEnd(situation: Situation) = {
+    val pieces = situation.board.piecesOf(situation.color)
+    Role.all exists { role => pieces forall { _._2.isNot(role) } }
+  }
+
+  // the king is an ordinary piece in extinction chess, so we don't filter
+  // promotion of your last pawn loses the game, so it is not allowed
+  // that logic will go in here for now (although it's pretty unrelated to "king" safety)
+  override def kingSafety(m: Move, filter: Piece => Boolean, kingPos: Option[Pos]): Boolean = {
+    (m.promotion.isEmpty || (m.after.piecesOf(m.piece.color) exists(_._2.is(Pawn))))
+  }
+
+  // castling out of, through, and into check is allowed
+  override def kingThreatened(board: Board, color: Color, to: Pos, filter: Piece => Boolean = _ => true): Boolean = false
+
+  override def promotableRoles: List[PromotableRole] = List(Queen, Rook, Bishop, Knight, King)
+
+}

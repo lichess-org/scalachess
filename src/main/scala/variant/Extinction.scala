507bbb06f8d767400c8c40c6648bbfbf96fd1310
stevenhao <stevenhao@gmail.com>
2016-02-06 10:32:35 -0500

    clean up style

--- src/main/scala/variant/Extinction.scala
+++ src/main/scala/variant/Extinction.scala
@@ -8,24 +8,23 @@ case object Extinction extends Variant(
   key = "extinct",
   name = "Extinction",
   shortName = "extinct",
-  title = "Capture a piece to extinction to win the game.",
+  title = "Capture any piece species to extinction to win the game.",
   standardInitialPosition = true) {
 
   override def specialEnd(situation: Situation) = {
     val pieces = situation.board.piecesOf(situation.color)
-    Role.all exists { role => pieces forall { _._2.isNot(role) } }
+    Role.all exists(role => pieces forall(_._2.isNot(role)))
   }
 
-  // the king is an ordinary piece in extinction chess, so we don't filter
+  // the king is an ordinary piece in extinction chess, so we don't worry about it
   // promotion of your last pawn loses the game, so it is not allowed
-  // that logic will go in here for now (although it's pretty unrelated to "king" safety)
-  override def kingSafety(m: Move, filter: Piece => Boolean, kingPos: Option[Pos]): Boolean = {
-    (m.promotion.isEmpty || (m.after.piecesOf(m.piece.color) exists(_._2.is(Pawn))))
-  }
+  override def kingSafety(m: Move, filter: Piece => Boolean, kingPos: Option[Pos]): Boolean =
+    m.promotion.isEmpty || (m.after.piecesOf(m.piece.color) exists(_._2.is(Pawn)))
 
-  // castling out of, through, and into check is allowed
+  // castling out of, through, and into check are all allowed
   override def kingThreatened(board: Board, color: Color, to: Pos, filter: Piece => Boolean = _ => true): Boolean = false
 
+  // you can promote to king
   override def promotableRoles: List[PromotableRole] = List(Queen, Rook, Bishop, Knight, King)
 
 }

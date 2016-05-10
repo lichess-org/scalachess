3b07a1af90634a2e6ee9c4cbf7d04020ff8278df
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-05-10 16:36:19 +0200

    fix chess glyphs

--- src/main/scala/format/pgn/Glyph.scala
+++ src/main/scala/format/pgn/Glyph.scala
@@ -45,19 +45,19 @@ object Glyph {
     val mistake = new Glyph(2, "?", "Mistake") with MoveAssessment
     val brillant = new Glyph(3, "!!", "Brillant move") with MoveAssessment
     val blunder = new Glyph(4, "??", "Blunder") with MoveAssessment
-    val speculative = new Glyph(5, "!?", "Speculative move") with MoveAssessment
-    val questionable = new Glyph(6, "?!", "Questionable move") with MoveAssessment
+    val interesting = new Glyph(5, "!?", "Interesting move") with MoveAssessment
+    val dubious = new Glyph(6, "?!", "Dubious move") with MoveAssessment
     val only = new Glyph(7, "□", "Only move") with MoveAssessment
     val singular = new Glyph(8, "µ", "Singular move") with MoveAssessment
 
-    val all = List(good, mistake, brillant, blunder, speculative, questionable, only, singular)
+    val all = List(good, mistake, brillant, blunder, interesting, dubious, only, singular)
     val byId = all.map { g => g.id -> g }.toMap
   }
 
   sealed trait PositionAssessment extends Glyph
 
   object PositionAssessment {
-    val equal = new Glyph(10, "=", "Even position") with PositionAssessment
+    val equal = new Glyph(10, "=", "Equal position") with PositionAssessment
     val unclear = new Glyph(13, "∞", "Unclear position") with PositionAssessment
     val whiteSlightlyBetter = new Glyph(14, "⩲", "White is slightly better") with PositionAssessment
     val blackSlightlyBetter = new Glyph(15, "⩱", "Black is slightly better") with PositionAssessment
@@ -75,8 +75,8 @@ object Glyph {
   object Observation {
     val zugzwang = new Glyph(22, "⨀", "Zugzwang") with Observation
     val development = new Glyph(32, "↑↑", "Development") with Observation
-    val initiative = new Glyph(36, "→", "Initiative") with Observation
-    val attack = new Glyph(40, "↑", "Attack") with Observation
+    val initiative = new Glyph(36, "↑", "Initiative") with Observation
+    val attack = new Glyph(40, "→", "Attack") with Observation
     val counterplay = new Glyph(132, "⇆", "Counterplay") with Observation
     val timeTrouble = new Glyph(138, "⊕", "Time trouble") with Observation
     val novelty = new Glyph(146, "N", "Novelty") with Observation

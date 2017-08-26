d5137439a29fb86b6c66af4db2124be1e1e844de
Isaac Levy <isaac.r.levy@gmail.com>
2017-08-25 21:25:40 -0400

    Update lag estimation
    - Using arithmatic mean instead of geometric, to be more
      tolerant of lag spikes when producing estimations.
    - Adjust quota start and max based on scaled quota gains.
      This will defacto reduce lag comp for bullet games.

--- src/main/scala/DecayingStats.scala
+++ src/main/scala/DecayingStats.scala
@@ -2,9 +2,8 @@ package chess
 
 final case class DecayingStats(
     mean: Float,
-    variance: Float,
-    decay: Float,
-    decayVar: Float
+    deviation: Float,
+    decay: Float
 ) {
   def record[T](values: Traversable[T])(implicit n: Numeric[T]): DecayingStats =
     values.foldLeft(this) { (s, v) => s record n.toFloat(v) }
@@ -14,19 +13,16 @@ final case class DecayingStats(
 
     copy(
       mean = value + decay * delta,
-      variance = decayVar * variance + (1 - decayVar) * delta * delta
+      deviation = decay * deviation + (1 - decay) * math.abs(delta)
     )
   }
-
-  def stdDev = Math.sqrt(variance).toFloat
 }
 
 object DecayingStats {
-  def empty(variance: Float, decay: Float = 0.9f)(value: Float) =
+  def empty(deviation: Float, decay: Float = 0.9f)(value: Float) =
     DecayingStats(
       mean = value,
-      variance = variance,
-      decay = decay,
-      decayVar = decay * Math.sqrt(decay).toFloat
+      deviation = deviation,
+      decay = decay
     )
 }

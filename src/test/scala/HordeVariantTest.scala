24444680455c53cd7adbc91bc561b53873eb8a2b
Daniel Dugovic <dandydand@gmail.com>
2017-11-19 06:29:19 -0600

    Add unit test for horde simple fortress versus king and pawns

--- src/test/scala/HordeVariantTest.scala
+++ src/test/scala/HordeVariantTest.scala
@@ -6,8 +6,8 @@ class HordeVariantTest extends ChessTest {
 
   "Horde chess" should {
 
-    "Must recognise insufficient winning material for white with 1 pawn left." in {
-      val position = "8/2k5/3q4/8/8/8/1P6/8 w - -"
+    "Must recognise insufficient winning material for horde with simple fortress." in {
+      val position = "8/pk6/P7/P7/P7/P7/P7/P7 b - -"
       val game = fenToGame(position, Horde)
 
       game must beSuccess.like {
@@ -16,8 +16,18 @@ class HordeVariantTest extends ChessTest {
       }
     }
 
-    "Must recognise insufficient winning material for white with 1 queen left." in {
-      val position = "8/2k5/3q4/8/8/1Q6/8/8 w - -"
+    "Must recognise insufficient winning material for horde with only 1 pawn left." in {
+      val position = "8/2k5/3q4/8/8/8/1P6/8 b - -"
+      val game = fenToGame(position, Horde)
+
+      game must beSuccess.like {
+        case game =>
+          game.situation.board.variant.insufficientWinningMaterial(game.situation.board, Color.white) must beTrue
+      }
+    }
+
+    "Must recognise insufficient winning material for horde with only 1 queen left." in {
+      val position = "8/2k5/3q4/8/8/1Q6/8/8 b - -"
       val game = fenToGame(position, Horde)
 
       game must beSuccess.like {
@@ -27,8 +37,8 @@ class HordeVariantTest extends ChessTest {
 
     }
 
-    "Must recognise insufficient winning material for white with only 2 minor pieces left" in {
-      val position = "8/2k5/3q4/8/8/1B2N3/8/8 w - -"
+    "Must recognise insufficient winning material for horde with only 2 minor pieces left" in {
+      val position = "8/2k5/3q4/8/8/1B2N3/8/8 b - -"
       val game = fenToGame(position, Horde)
 
       game must beSuccess.like {
@@ -37,8 +47,8 @@ class HordeVariantTest extends ChessTest {
       }
     }
 
-    "Must not be insufficient winning material with 3 minor pieces left" in {
-      val position = "8/2k5/3q4/8/8/3B4/4NB2/8 w - -"
+    "Must not be insufficient winning material for horde with 3 minor pieces left" in {
+      val position = "8/2k5/3q4/8/8/3B4/4NB2/8 b - -"
       val game = fenToGame(position, Horde)
 
       game must beSuccess.like {
@@ -47,7 +57,7 @@ class HordeVariantTest extends ChessTest {
       }
     }
 
-    "Must not auto-draw in B vs K endgame, black can win" in {
+    "Must not auto-draw in B vs K endgame, king can win" in {
       val position = "7B/6k1/8/8/8/8/8/8 b - -"
       val game = fenToGame(position, Horde)
 

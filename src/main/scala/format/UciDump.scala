fb30163e137cc66d37298d2224da70586daefb4d
Benedikt Werner <1benediktwerner@gmail.com>
2021-07-25 10:56:00 +0200

    Allow forcing 960 UCI notation in UCIDump

--- src/main/scala/format/UciDump.scala
+++ src/main/scala/format/UciDump.scala
@@ -8,23 +8,25 @@ import chess.variant.Variant
 object UciDump {
 
   // a2a4, b8c6
-  def apply(replay: Replay): List[String] =
-    replay.chronoMoves map move(replay.setup.board.variant)
+  def apply(force960Notation: Boolean = false)(replay: Replay): List[String] =
+    replay.chronoMoves map move(replay.setup.board.variant, force960Notation)
 
   def apply(
       moves: Seq[String],
       initialFen: Option[FEN],
-      variant: Variant
+      variant: Variant,
+      force960Notation: Boolean = false
   ): Validated[String, List[String]] =
     if (moves.isEmpty) Validated.valid(Nil)
-    else Replay(moves, initialFen, variant) andThen (_.valid) map apply
+    else Replay(moves, initialFen, variant) andThen (_.valid) map apply(force960Notation)
 
-  def move(variant: Variant)(mod: MoveOrDrop): String =
+  def move(variant: Variant, force960Notation: Boolean = false)(mod: MoveOrDrop): String =
     mod match {
       case Left(m) =>
         m.castle.fold(m.toUci.uci) {
-          case ((kf, kt), (rf, _)) if kf == kt || variant.chess960 || variant.fromPosition => kf.key + rf.key
-          case ((kf, kt), _)                                                               => kf.key + kt.key
+          case ((kf, kt), (rf, _))
+              if force960Notation || kf == kt || variant.chess960 || variant.fromPosition => kf.key + rf.key
+          case ((kf, kt), _)                                                              => kf.key + kt.key
         }
       case Right(d) => d.toUci.uci
     }

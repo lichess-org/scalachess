2b6a5eee26f75e977cd90c28b4e643eb00b57fc9
Benedikt Werner <1benediktwerner@gmail.com>
2021-10-30 00:44:29 +0200

    Convert chess.com 3-check PGN tag

--- src/main/scala/format/pgn/tagModel.scala
+++ src/main/scala/format/pgn/tagModel.scala
@@ -34,6 +34,7 @@ case class Tags(value: List[Tag]) extends AnyVal {
   def variant: Option[chess.variant.Variant] =
     apply(_.Variant).map(_.toLowerCase).flatMap {
       case "chess 960" | "fischerandom" | "fischerrandom" => chess.variant.Chess960.some
+      case "3 check" | "3-check"                          => chess.variant.ThreeCheck.some
       case name                                           => chess.variant.Variant byName name
     }
 

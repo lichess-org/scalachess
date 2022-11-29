55eedc87abac111ae6e1f7cf722130ce9296532b
Thibault Duplessis <t@lichess.org>
2022-11-29 20:59:21 +0100

    upgrade scalalib

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,12 +1,8 @@
 package chess
 
-object Chess extends ornicar.scalalib.Zeros
-
-export Chess.{ *, given }
-
-export ornicar.scalalib.OrnicarBooleanWrapper
-export ornicar.scalalib.ScalalibExtensions.*
 export ornicar.scalalib.newtypes.*
+export ornicar.scalalib.zeros.*
+export ornicar.scalalib.extensions.*
 
 export Color.{ Black, White }
 

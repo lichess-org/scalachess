98832d9156dcd9cf2e38b33cb343bc5483dfba36
Thibault Duplessis <t@lichess.org>
2022-11-29 16:42:14 +0100

    upgrade scalalib

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -5,9 +5,6 @@ import cats.data.Validated
 export ornicar.scalalib.ScalalibExtensions.*
 export ornicar.scalalib.OrnicarBooleanWrapper
 
-extension [E, A](validated: Validated[E, A])
-  def flatMap[EE >: E, B](f: A => Validated[EE, B]): Validated[EE, B] = validated.andThen(f)
-
 export Color.{ Black, White }
 
 type Direction  = Pos => Option[Pos]

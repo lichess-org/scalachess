35d835e14ae7f4a05b0bd10db27063e3e6f24d20
Thibault Duplessis <t@lichess.org>
2022-11-29 22:35:28 +0100

    improve Centis

--- src/main/scala/Centis.scala
+++ src/main/scala/Centis.scala
@@ -18,12 +18,11 @@ object Centis extends OpaqueInt[Centis]:
     def roundTenths: Int  = if (centis > 0) (centis + 5) / 10 else (centis - 4) / 10
     def roundSeconds: Int = Math.round(centis * 0.01f)
 
-    // def toSeconds: BigDecimal = java.math.BigDecimal.valueOf(centis, 2)
-    inline def millis: Long  = centis * 10L
-    def toDuration: Duration = FiniteDuration(millis, MILLISECONDS)
+    inline def toSeconds: BigDecimal = java.math.BigDecimal.valueOf(centis, 2)
+    inline def millis: Long          = centis * 10L
+    def toDuration: FiniteDuration   = FiniteDuration(millis, MILLISECONDS)
 
-    def *~(scalar: Float): Centis = ofFloat(scalar * centis)
-    // def *~(scalar: Double): Centis  = Centis(scalar * centis)
+    def *~(scalar: Float): Centis   = ofFloat(scalar * centis)
     def /(div: Int): Option[Centis] = div != 0 option (centis / div)
 
     def avg(other: Centis): Centis = (centis + other.value) >> 1
@@ -38,10 +37,6 @@ object Centis extends OpaqueInt[Centis]:
     def combine(c1: Centis, c2: Centis) = c1 + c2
     val empty                           = 0
 
-  // extension (scalar: Int) def *(o: Centis) = o * scalar
-  // extension (scalar: Float) def *~(o: Centis) = o *~ scalar
-  // extension (scalar: Double) def *~(o: Centis) = o *~ scalar
-
   def ofLong(l: Long): Centis =
     if (l.toInt == l) l.toInt
     else if (l > 0) Integer.MAX_VALUE
@@ -53,9 +48,8 @@ object Centis extends OpaqueInt[Centis]:
       else d.toMillis
     }
 
-  inline def ofFloat(f: Float): Centis = Math.round(f)
-  // def ofDouble(d: Double): Centis = Math.round(d)
+  inline def ofFloat(f: Float): Centis   = Math.round(f)
+  inline def ofDouble(d: Double): Centis = ofLong(Math.round(d))
 
   inline def ofSeconds(s: Int): Centis = 100 * s
-  // def ofMillis(i: Int): Centis  = (if (i > 0) i + 5 else i - 4) / 10
   inline def ofMillis(l: Long): Centis = ofLong(if (l > 0) l + 5 else l - 4) / 10

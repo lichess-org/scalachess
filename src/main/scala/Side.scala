595d57ba3b016ef25a95cfbc9b0e74a6a6181575
Thibault Duplessis <t@lichess.org>
2022-12-11 14:05:21 +0100

    tweak Side enum

--- src/main/scala/Side.scala
+++ src/main/scala/Side.scala
@@ -3,17 +3,17 @@ package chess
 enum Side:
   case KingSide, QueenSide
 
-  inline def fold[A](k: => A, q: => A): A = if (king) k else q
+  inline def fold[A](k: => A, q: => A): A = if isKingSide then k else q
 
   lazy val castledKingFile: File = fold(File.G, File.C)
   lazy val castledRookFile: File = fold(File.F, File.D)
 
-  def tripToRook: (Pos, Board) => List[Pos] = (pos, board) =>
-    this match
-      case Side.KingSide => pos >| board.pieces.contains
-      case Side.QueenSide => pos |< board.pieces.contains
+  lazy val tripToRook: (Pos, Board) => List[Pos] = fold(
+    (pos, board) => pos >| board.pieces.contains,
+    (pos, board) => pos |< board.pieces.contains
+  )
 
-  private lazy val king = this == Side.KingSide
+  private lazy val isKingSide = this == Side.KingSide
 
 object Side:
 

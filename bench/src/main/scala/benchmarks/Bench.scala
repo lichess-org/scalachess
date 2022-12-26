3633ee899c41fdd0c747c9cba3c9f126e3806d57
Thanh Le <lenguyenthanh@hotmail.com>
2022-12-26 12:23:27 +0100

    Refactor sbt, add bench module, jmh plugin

---
+++ bench/src/main/scala/benchmarks/Bench.scala
@@ -0,0 +1,18 @@
+package benchmarks
+import org.openjdk.jmh.annotations._
+
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Measurement(iterations = 15, timeUnit = TimeUnit.SECONDS, time = 3)
+@Warmup(iterations = 15, timeUnit = TimeUnit.SECONDS, time = 3)
+@Threads(value = 1)
+class Bench {
+
+  @Benchmark
+  def justBench(): Int =
+    List.range(1,10000).sum
+
+}

339d88edb94721b676aa752ba99ab09691737802
Thanh Le <lenguyenthanh@hotmail.com>
2022-12-30 15:17:13 +0100

    Re-enable tests + fmt

--- src/test/scala/perft/Parser.scala
+++ src/test/scala/perft/Parser.scala
@@ -21,25 +21,24 @@ import chess.format.EpdFen
 object Parser:
   def parse: String => Either[P.Error, List[Perft]] = perfts.parseAll
 
-  private val whitespace = R.cr | R.lf | R.wsp
-  private val blank = P.until(!whitespace)
+  private val whitespace  = R.cr | R.lf | R.wsp
+  private val blank       = P.until(!whitespace)
   private val nonNegative = N.nonNegativeIntString
 
   private val comment = (P.caret.filter(_.col == 0) *> P.char('#')).endWith(R.lf)
   private val ignored = (comment | blank).void
 
-  private val id: P[String] = "id".prefix
+  private val id: P[String]  = "id".prefix
   private val epd: P[EpdFen] = "epd".prefix.map(EpdFen.clean)
-  private val testCase: P[TestCase] = ((nonNegative.map(_.toInt) <* P.char(' ')) ~ nonNegative.map(_.toLong)).map(TestCase.apply)
+  private val testCase: P[TestCase] =
+    ((nonNegative.map(_.toInt) <* P.char(' ')) ~ nonNegative.map(_.toLong)).map(TestCase.apply)
   private val oneTestCase: P[TestCase] = P.string("perft ") *> testCase <* R.lf.?
   private val cases: P[List[TestCase]] = oneTestCase.rep.map(_.toList) <* (ignored.rep | R.lf.rep0)
-  private val perft: P[Perft] = (id, epd, cases).mapN(Perft.apply) <* R.lf.?
+  private val perft: P[Perft]          = (id, epd, cases).mapN(Perft.apply) <* R.lf.?
   // private val perfts = perft.rep.map(_.toList)
   private val perfts = ignored.rep0 *> perft.rep.map(_.toList)
-
 
   extension (p: P0[Any])
     private def endWith(p1: P[Any]): P[String] = p.with1 *> (p1.string | (P.until(p1) <* p1))
 
-  extension (str: String)
-    private def prefix: P[String] = P.string(s"$str ").endWith(R.lf)
+  extension (str: String) private def prefix: P[String] = P.string(s"$str ").endWith(R.lf)
--- src/test/scala/perft/PerftTest.scala
+++ src/test/scala/perft/PerftTest.scala
@@ -22,12 +22,12 @@ class PerftTest extends ChessTest:
       }
     }
 
-  // genTests("calculate ThreeCheck perfts", Perft.read("3check.perft"), ThreeCheck, 1_000_000L)
-  // genTests("calculate Antichess perfts", Perft.read("antichess.perft"), Antichess, 1_000_000L)
-  // genTests("calculate Atomic perfts", Perft.read("atomic.perft"), Atomic, 1_000_000L)
-  // genTests("calculate Crazyhouse perfts", Perft.read("crazyhouse.perft"), Crazyhouse, 1_000_000L)
-  // genTests("calculate Horde perfts", Perft.read("horde.perft"), Horde, 1_000_000L)
-  // genTests("calculate RacingKings perfts", Perft.read("racingkings.perft"), RacingKings, 1_000_000L)
-  // genTests("calculate random perfts", Perft.read("random.perft"), Chess960, 10_000L)
-  // genTests("calculate tricky perfts", Perft.read("tricky.perft"), Chess960, 100_000L)
+  genTests("calculate ThreeCheck perfts", Perft.read("3check.perft"), ThreeCheck, 1_000_000L)
+  genTests("calculate Antichess perfts", Perft.read("antichess.perft"), Antichess, 1_000_000L)
+  genTests("calculate Atomic perfts", Perft.read("atomic.perft"), Atomic, 1_000_000L)
+  genTests("calculate Crazyhouse perfts", Perft.read("crazyhouse.perft"), Crazyhouse, 1_000_000L)
+  genTests("calculate Horde perfts", Perft.read("horde.perft"), Horde, 1_000_000L)
+  genTests("calculate RacingKings perfts", Perft.read("racingkings.perft"), RacingKings, 1_000_000L)
+  genTests("calculate random perfts", Perft.read("random.perft"), Chess960, 10_000L)
+  genTests("calculate tricky perfts", Perft.read("tricky.perft"), Chess960, 100_000L)
   genTests("calculate chess960 perfts", Perft.read("x-fen.perft"), Chess960, 10_000L)

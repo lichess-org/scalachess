77235fec7156d6761ad3d8244bea10ba27adcee6
Thanh Le <lenguyenthanh@hotmail.com>
2022-12-30 20:57:50 +0100

    Code golf

--- src/test/scala/perft/FullRandomPerftTest.scala
+++ src/test/scala/perft/FullRandomPerftTest.scala
@@ -12,11 +12,14 @@ import cats.kernel.Monoid
 
 object FullRandomPerftTest extends SimpleIOSuite:
 
+  given Monoid[Boolean] with
+    def empty = true
+    def combine(x: Boolean, y: Boolean) = x && y
+
   // Tests all perft scenario on random.perft with node limitation is 10_000
   test("random.perft") {
     Perft.read("random.perft")
-      .parTraverse(perft => IO(genTests(perft, Chess960, 10_000L)))
-      .map(_.forall(identity))
+      .parFoldMapA(perft => IO(genTests(perft, Chess960, 10_000L)))
       .map(assert(_))
   }
 

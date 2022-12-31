33c723a13148c0a238b4dd6675f9e8bf2fa41e29
Thanh Le <lenguyenthanh@hotmail.com>
2022-12-31 18:50:43 +0100

    Optimize 3check variant check with bitboard

--- src/main/scala/bitboard/Board.scala
+++ src/main/scala/bitboard/Board.scala
@@ -216,6 +216,9 @@ case class Board(
   inline def hasPiece(inline p: Piece) =
     piece(p).nonEmpty
 
+  lazy val onlyKingsLeft = kings == occupied
+  def isOnlyKingLeft(color: Color) = (kings & byColor(color)) == byColor(color)
+
   // TODO remove unsafe get
   // we believe in the integrity of bitboard
   // tests pieceMap . fromMap = identity

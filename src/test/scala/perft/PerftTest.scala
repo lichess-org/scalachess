82c1bf05abc7106519afb88b1841a608e6e4694e
Thanh Le <lenguyenthanh@hotmail.com>
2023-01-21 02:31:07 +0100

    Increase node limit for perf tests

--- src/test/scala/perft/PerftTest.scala
+++ src/test/scala/perft/PerftTest.scala
@@ -31,5 +31,5 @@ class PerftTest extends ChessTest:
   genTests("calculate RacingKings perfts", Perft.racingkingsPerfts, RacingKings, 1_000_000L)
   // for the shake of time we only test the first 50 cases in random.peft, run FullRandomPerftTest.scala for all cases
   genTests("calculate random perfts", Perft.randomPerfts.take(50), Chess960, 10_000L)
-  genTests("calculate tricky perfts", Perft.trickyPerfts, Chess960, 100_000L)
-  genTests("calculate chess960 perfts", Perft.chess960, Chess960, 100_000L)
+  genTests("calculate tricky perfts", Perft.trickyPerfts, Chess960, 1_000_000L)
+  genTests("calculate chess960 perfts", Perft.chess960, Chess960, 1_000_000L)

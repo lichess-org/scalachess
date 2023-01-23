272abbab6b0347f12b9fb21606cf890f7f721cfa
Thanh Le <lenguyenthanh@hotmail.com>
2023-01-23 10:35:26 +0700

    Use nodeLimits variable for perft tests

--- src/test/scala/perft/PerftTest.scala
+++ src/test/scala/perft/PerftTest.scala
@@ -22,14 +22,15 @@ class PerftTest extends ChessTest:
       }
     }
 
-  genTests("calculate ThreeCheck perfts", Perft.threeCheckPerfts, ThreeCheck, 10_000_000L)
-  genTests("calculate Antichess perfts", Perft.antichessPerfts, Antichess, 10_000_000L)
-  genTests("calculate Atomic perfts", Perft.atomicPerfts, Atomic, 10_000_000L)
+  val nodeLimits = 1_000_000L
+  genTests("calculate ThreeCheck perfts", Perft.threeCheckPerfts, ThreeCheck, nodeLimits)
+  genTests("calculate Antichess perfts", Perft.antichessPerfts, Antichess, nodeLimits)
+  genTests("calculate Atomic perfts", Perft.atomicPerfts, Atomic, nodeLimits)
   // TODO scalachess doesn't generate drops now
   // genTests("calculate Crazyhouse perfts", Perft.crazyhousePerfts, Crazyhouse, 10_000_000L)
-  genTests("calculate Horde perfts", Perft.hordePerfts, Horde, 10_000_000L)
-  genTests("calculate RacingKings perfts", Perft.racingkingsPerfts, RacingKings, 10_000_000L)
+  genTests("calculate Horde perfts", Perft.hordePerfts, Horde, nodeLimits)
+  genTests("calculate RacingKings perfts", Perft.racingkingsPerfts, RacingKings, nodeLimits)
   // for the shake of time we only test the first 50 cases in random.peft, run FullRandomPerftTest.scala for all cases
-  genTests("calculate random perfts", Perft.randomPerfts.take(100), Chess960, 10_000_000L)
-  genTests("calculate tricky perfts", Perft.trickyPerfts, Chess960, 10_000_000L)
-  genTests("calculate chess960 perfts", Perft.chess960, Chess960, 10_000_000L)
+  genTests("calculate random perfts", Perft.randomPerfts.take(100), Chess960, nodeLimits)
+  genTests("calculate tricky perfts", Perft.trickyPerfts, Chess960, nodeLimits)
+  genTests("calculate chess960 perfts", Perft.chess960, Chess960, nodeLimits)

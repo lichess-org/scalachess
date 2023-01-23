38d688f399a04b9e4cd462e9babf8d5fa66c61e6
Thanh Le <lenguyenthanh@hotmail.com>
2023-01-23 13:34:47 +0700

    Disable PerftTest
    We now have FullPerftTest, which cover this one.
    But for FullPerftTest we only know if one of the test failed.
    If that the case, we should use PerftTest to identify which is failed.

--- src/test/scala/perft/PerftTest.scala
+++ src/test/scala/perft/PerftTest.scala
@@ -22,15 +22,15 @@ class PerftTest extends ChessTest:
       }
     }
 
-  val nodeLimits = 100_000L
-  genTests("calculate ThreeCheck perfts", Perft.threeCheckPerfts, ThreeCheck, nodeLimits)
-  genTests("calculate Antichess perfts", Perft.antichessPerfts, Antichess, nodeLimits)
-  genTests("calculate Atomic perfts", Perft.atomicPerfts, Atomic, nodeLimits)
-  // TODO scalachess doesn't generate drops now
-  // genTests("calculate Crazyhouse perfts", Perft.crazyhousePerfts, Crazyhouse, nodeLimits)
-  genTests("calculate Horde perfts", Perft.hordePerfts, Horde, nodeLimits)
-  genTests("calculate RacingKings perfts", Perft.racingkingsPerfts, RacingKings, nodeLimits)
-  // for the shake of time we only test the first 50 cases in random.peft, run FullRandomPerftTest.scala for all cases
-  genTests("calculate random perfts", Perft.randomPerfts.take(100), Chess960, nodeLimits)
-  genTests("calculate tricky perfts", Perft.trickyPerfts, Chess960, nodeLimits)
-  genTests("calculate chess960 perfts", Perft.chess960, Chess960, nodeLimits)
+  // val nodeLimits = 1_000_000L
+  // genTests("calculate ThreeCheck perfts", Perft.threeCheckPerfts, ThreeCheck, nodeLimits)
+  // genTests("calculate Antichess perfts", Perft.antichessPerfts, Antichess, nodeLimits)
+  // genTests("calculate Atomic perfts", Perft.atomicPerfts, Atomic, nodeLimits)
+  // // TODO scalachess doesn't generate drops now
+  // // genTests("calculate Crazyhouse perfts", Perft.crazyhousePerfts, Crazyhouse, nodeLimits)
+  // genTests("calculate Horde perfts", Perft.hordePerfts, Horde, nodeLimits)
+  // genTests("calculate RacingKings perfts", Perft.racingkingsPerfts, RacingKings, nodeLimits)
+  // // for the shake of time we only test the first 50 cases in random.peft, run FullRandomPerftTest.scala for all cases
+  // genTests("calculate random perfts", Perft.randomPerfts.take(100), Chess960, nodeLimits)
+  // genTests("calculate tricky perfts", Perft.trickyPerfts, Chess960, nodeLimits)
+  // genTests("calculate chess960 perfts", Perft.chess960, Chess960, nodeLimits)

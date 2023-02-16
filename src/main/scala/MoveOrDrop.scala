1ec97769abea4c722bae01138acfaf1eaf4fea97
Thanh Le <lenguyenthanh@hotmail.com>
2023-02-16 15:10:23 +0100

    inline functions

--- src/main/scala/MoveOrDrop.scala
+++ src/main/scala/MoveOrDrop.scala
@@ -9,7 +9,7 @@ object MoveOrDrop:
     def isMove = md.isInstanceOf[Move]
     def isDrop = md.isInstanceOf[Drop]
 
-    def fold[A](move: Move => A, drop: Drop => A): A =
+    inline def fold[A](move: Move => A, drop: Drop => A): A =
       md match
         case m: Move => move(m)
         case d: Drop => drop(d)
@@ -17,12 +17,12 @@ object MoveOrDrop:
     def move: Option[Move] = md.fold(Some(_), _ => None)
     def drop: Option[Drop] = md.fold(_ => None, Some(_))
 
-    def applyVariantEffect: MoveOrDrop =
+    inline def applyVariantEffect: MoveOrDrop =
       md match
         case m: Move => m.applyVariantEffect
         case d: Drop => d
 
-    def toUci: Uci =
+    inline def toUci: Uci =
       md match
         case m: Move => m.toUci
         case d: Drop => d.toUci

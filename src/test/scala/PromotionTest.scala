641eb346c04f8e125dc9f0ffcdf4ea32be5e539c
Thanh Le <lenguyenthanh@hotmail.com>
2023-02-22 22:24:00 +0100

    Remove commented out tests
    Which are not relevant anymore

--- src/test/scala/PromotionTest.scala
+++ src/test/scala/PromotionTest.scala
@@ -9,68 +9,6 @@ import chess.variant.Standard
 
 class PromotionTest extends ChessTest:
 
-//   "pawn promotion" should {
-//     val board = """
-//     k
-//   p
-// K      """
-//     val game  = Game(board, Black)
-//     "promote to a queen" in {
-//       game.playMove(C2, C1, Queen.some) must beGame("""
-//         k
-//
-// K q    """)
-//     }
-//     "promote to a queen by default" in {
-//       game.playMove(C2, C1) must beGame("""
-//
-// K q    """)
-//     }
-//     "promote to a knight" in {
-//       game.playMove(C2, C1, Knight.some) must beGame("""
-//
-// K n    """)
-//     }
-//     "promote to a queen by killing" in {
-//       Game(
-//         """
-//   p
-// K  R""",
-//         Black
-//       ).playMove(C2, D1) must beGame("""
-//
-// K  q""")
-//     }
-//     "promote to a knight by killing" in {
-//       Game(
-//         """
-//   p
-// K  R""",
-//         Black
-//       ).playMove(C2, D1, Knight.some) must beGame("""
-//
-// K  n""")
-//     }
-//     "promote to a whiteknight" in {
-//       Game("""
-//
-// P
-//
-//
-//
-//
-//
-// K n    """).playMove(A7, A8, Knight.some) must beGame("""
-// N
-//
-//
-//
-//
-//
-//
-// K n    """)
-//     }
-//
   "Not allow promotion to a king in a standard game " in {
     val fen  = EpdFen("8/1P6/8/8/8/8/7k/1K6 w - -")
     val game = fenToGame(fen, Standard)
@@ -79,5 +17,3 @@ class PromotionTest extends ChessTest:
 
     failureGame must beInvalid
   }
-
-// }
--- src/test/scala/QueenTest.scala
+++ src/test/scala/QueenTest.scala
@@ -9,64 +9,6 @@ class QueenTest extends ChessTest:
 
     val queen = White - Queen
 
-    // "move in any direction until the edge of the board" in {
-    //   pieceMoves(queen, D4) must bePoss(
-    //     D5,
-    //     D6,
-    //     D7,
-    //     D8,
-    //     D3,
-    //     D2,
-    //     D1,
-    //     E4,
-    //     F4,
-    //     G4,
-    //     H4,
-    //     C4,
-    //     B4,
-    //     A4,
-    //     C3,
-    //     B2,
-    //     A1,
-    //     E5,
-    //     F6,
-    //     G7,
-    //     H8,
-    //     C5,
-    //     B6,
-    //     A7,
-    //     E3,
-    //     F2,
-    //     G1
-    //   )
-    // }
-
-    // "move 1 position in any direction, even from the edges" in {
-    //   pieceMoves(queen, H8) must bePoss(
-    //     H7,
-    //     H6,
-    //     H5,
-    //     H4,
-    //     H3,
-    //     H2,
-    //     H1,
-    //     G7,
-    //     F6,
-    //     E5,
-    //     D4,
-    //     C3,
-    //     B2,
-    //     A1,
-    //     G8,
-    //     F8,
-    //     E8,
-    //     D8,
-    //     C8,
-    //     B8,
-    //     A8
-    //   )
-    // }
-
     "not move to positions that are occupied by the same colour" in {
       val board = """
 k B

6effc4f3dbe8ad122792da938e70bf9c66cac2ca
Thanh Le <lenguyenthanh@hotmail.com>
2023-03-19 20:44:46 +0100

    Use new algorithm for Horde

--- src/main/scala/variant/Horde.scala
+++ src/main/scala/variant/Horde.scala
@@ -76,47 +76,8 @@ case object Horde
     * this method does not detect; however, such are trivial to premove.
     */
   override def opponentHasInsufficientMaterial(situation: Situation): Boolean =
-    val board         = situation.board
-    val opponentColor = !situation.color
-    lazy val fortress = hordeClosedPosition(situation) // costly function call
-    if opponentColor == Color.white then
-      lazy val notKingPieces           = InsufficientMatingMaterial.nonKingPieces(board) toList
-      val horde                        = board.piecesOf(Color.white)
-      lazy val hordeBishopSquareColors = horde.filter(_._2.is(Bishop)).toList.map(_._1.isLight).distinct
-      lazy val hordeRoles              = horde.map(_._2.role)
-      lazy val army                    = board.piecesOf(Color.black)
-      lazy val armyPawnsOrRooks        = army.count(p => p._2.is(Pawn) || p._2.is(Rook))
-      lazy val armyPawnsOrBishops      = army.filter(p => p._2.is(Pawn) || p._2.is(Bishop))
-      lazy val armyPawnsOrKnights      = army.count(p => p._2.is(Pawn) || p._2.is(Knight))
-      lazy val armyNonQueens           = army.count(_._2.isNot(Queen))
-      lazy val armyNonQueensOrRooks    = army.count(p => p._2.isNot(Queen) && p._2.isNot(Rook))
-      lazy val armyNonQueensOrBishops  = army.count(p => p._2.isNot(Queen) && p._2.isNot(Bishop))
-      lazy val armyBishopSquareColors  = army.filter(_._2.is(Bishop)).toList.map(_._1.isLight).distinct
-      if (horde.sizeIs == 1)
-        hordeRoles match
-          case List(Knight) =>
-            army.sizeIs < 4 || armyNonQueensOrRooks == 0 || armyNonQueensOrBishops == 0 || (armyNonQueensOrBishops + armyBishopSquareColors.size) < 4
-          case List(Bishop) =>
-            notKingPieces.count(p =>
-              p._2.is(Pawn) || (p._2.is(Bishop) && p._1.isLight != horde.head._1.isLight)
-            ) < 2
-          case List(Rook) => army.sizeIs < 3 || armyPawnsOrRooks == 0 || armyPawnsOrKnights == 0
-          case _          => armyPawnsOrRooks == 0
-      else if (
-        (hordeRoles.forall(
-          _ == Bishop
-        ) && hordeBishopSquareColors.lengthCompare(1) == 0) && {
-          armyPawnsOrKnights + armyPawnsOrBishops
-            .count(p => p._1.isLight != horde.head._1.isLight) < 2
-        }
-      ) true
-      else if (
-        horde.sizeIs == 2 && hordeRoles
-          .count(r => r == Queen || r == Rook || r == Pawn) < 2 && armyNonQueens <= 1
-      )
-        true
-      else fortress
-    else fortress
+    if hasInsufficientMaterial(situation.board, !situation.color) then true
+    else hordeClosedPosition(situation) // costly function call
 
   extension (board: Board)
     def hasBishopPair: Color => Boolean = side =>

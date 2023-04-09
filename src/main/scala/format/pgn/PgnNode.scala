e106c597239078c56cc247619d6b23287b198c93
Thanh Le <lenguyenthanh@hotmail.com>
2023-04-09 23:47:20 +0200

    Implement NewPgn as replacement for Pgn

--- src/main/scala/format/pgn/PgnNode.scala
+++ src/main/scala/format/pgn/PgnNode.scala
@@ -11,34 +11,33 @@ case class Node[A, B](
 ):
   def mainLine: List[A] = move :: child.fold(List.empty[A])(_.mainLine)
 
-case class PgnNodeData(san: San, metas: Metas)
-
 case class VariationNode[A, B](node: Node[A, B], extra: B)
 
-type PgnTree = Node[PgnNodeData, List[Comment]]
+case class PgnNodeData(san: San, metas: Metas)
+type ParsedPgnTree = Node[PgnNodeData, List[Comment]]
 
 // isomorphic to ParsedPgn
-case class NewPgn(initialPosition: InitialPosition, tags: Tags, tree: Option[PgnTree]):
+case class NewParsedPgn(initialPosition: InitialPosition, tags: Tags, tree: Option[ParsedPgnTree]):
   def toParsedPgn: ParsedPgn =
-    val sans = tree.fold(List.empty[San])(traverse)
+    val sans = tree.fold(List.empty[San])(toSan)
     ParsedPgn(initialPosition, tags, Sans(sans))
 
-  def traverse(node: PgnTree): List[San] =
-    val variations = node.variations.map(x => Variation(x.extra, Sans(traverse(x.node))))
+  def toSan(node: ParsedPgnTree): List[San] =
+    val variations = node.variations.map(x => Variation(x.extra, Sans(toSan(x.node))))
     val san        = node.move.san.withMetas(node.move.metas).withVariations(variations)
-    san :: node.child.fold(Nil)(traverse)
+    san :: node.child.fold(Nil)(toSan)
 
-object NewPgn:
+object NewParsedPgn:
   extension (san: San) def clean: San = san.withMetas(Metas.empty).withVariations(Nil)
 
   extension (pgn: ParsedPgn)
-    def toNewPgn: NewPgn =
-      val tree = pgn.sans.value.reverse.foldLeft(none[PgnTree]) { (o, san) =>
+    def toNewPgn: NewParsedPgn =
+      val tree = pgn.sans.value.reverse.foldLeft(none[ParsedPgnTree]) { (o, san) =>
         Some(toNode(san, o))
       }
-      NewPgn(initialPosition = pgn.initialPosition, tags = pgn.tags, tree = tree)
+      NewParsedPgn(initialPosition = pgn.initialPosition, tags = pgn.tags, tree = tree)
 
-  def toNode(san: San, child: Option[PgnTree]): PgnTree =
+  def toNode(san: San, child: Option[ParsedPgnTree]): ParsedPgnTree =
     Node(
       PgnNodeData(san.clean, san.metas),
       child,
@@ -46,9 +45,38 @@ object NewPgn:
     )
 
   def toVariationNode(sans: Sans, comments: List[Comment]) =
-    val node = sans.value.reverse.foldLeft(none[PgnTree]) { (o, san) =>
+    val node = sans.value.reverse.foldLeft(none[ParsedPgnTree]) { (o, san) =>
       o match
         case None       => Some(toNode(san, None))
         case Some(node) => Some(toNode(san, Some(node)))
     }
     node.map(VariationNode(_, comments))
+
+type PgnTree = Node[Move, Unit]
+// isomorphic to Pgn
+case class NewPgn(tags: Tags, initial: Initial, tree: Option[PgnTree]):
+  def toPgn: Pgn =
+    val moves = tree.fold(List.empty[Move])(toMove(_, Ply(1)))
+    val turns = Turn.fromMoves(moves, Ply(1))
+    Pgn(tags, turns, initial)
+
+  def toMove(node: PgnTree, ply: Ply): List[Move] =
+    val variations = node.variations.map(x => Turn.fromMoves(toMove(x.node, ply), ply))
+    val move       = node.move.copy(variations = variations)
+    move :: node.child.fold(Nil)(toMove(_, ply + 1))
+
+object NewPgn:
+  def moves(turn: Turn): List[Move] = List(turn.white, turn.black).flatten
+  def moves(pgn: Pgn): List[Move]   = pgn.turns.flatMap(moves)
+
+  extension (move: Move) def clean: Move = move.copy(variations = Nil)
+  def toNewPgn(pgn: Pgn): NewPgn =
+    val tree = moves(pgn).reverse.foldLeft(none[PgnTree]) { (o, move) => Some(toNode(move, o)) }
+    NewPgn(tags = pgn.tags, initial = pgn.initial, tree = tree)
+
+  def toNode(move: Move, child: Option[PgnTree]): PgnTree =
+    Node(
+      move.clean,
+      child,
+      move.variations.map(_.flatMap(moves)).map(x => VariationNode(toNode(x.head, None), ()))
+    )
---
+++ src/test/scala/format/pgn/PgnNodeTest.scala
@@ -0,0 +1,307 @@
+package chess
+package format.pgn
+
+import scala.language.implicitConversions
+
+import cats.syntax.all.*
+import munit.FunSuite
+import chess.format.pgn.Fixtures
+
+class PgnNodeTest extends FunSuite:
+
+  given Conversion[String, SanStr] = SanStr(_)
+
+  private def glyphs(id: Int) =
+    Glyph.find(id).fold(Glyphs.empty) { g =>
+      Glyphs fromList List(g)
+    }
+
+  test("NewParsePgn and ParsedPgn are isomorphic"):
+    Fixtures.gamesForPerfTest.foreach { str =>
+      val parsedPgn = Parser.full(str).valueOr(err => throw new Exception(err.toString))
+      val newPgn    = NewParsedPgn.toNewPgn(parsedPgn)
+      assertEquals(parsedPgn, newPgn.toParsedPgn)
+    }
+
+  val pgn1 = Pgn(
+    tags = Tags(
+      List(
+        Tag(_.White, "Kramnik,V"),
+        Tag(_.Black, "Anand,V"),
+        Tag(_.ECO, "D14")
+      )
+    ),
+    turns = List(
+      Turn(
+        number = 1,
+        white = Move("d4").some,
+        black = Move("d5").some
+      ),
+      Turn(
+        number = 2,
+        white = Move("c4", glyphs = glyphs(1)).some,
+        black = Move("c6", glyphs = glyphs(2)).some
+      ),
+      Turn(
+        number = 3,
+        white = Move("Nc3", glyphs = glyphs(3)).some,
+        black = Move("Nf6").some
+      ),
+      Turn(
+        number = 4,
+        white = Move(
+          "cxd5",
+          comments =
+            "The Exchange Slav, the sure way to play with zero losing chances so an ideal choice for game one" :: Nil
+        ).some,
+        black = Move("cxd5").some
+      ),
+      Turn(
+        number = 5,
+        white = Move("Bf4").some,
+        black = Move("Nc6").some
+      )
+    )
+  )
+
+  val pgn2 = Pgn(
+    tags = Tags(
+      List(
+        Tag(_.White, "tsinnema"),
+        Tag(_.Black, "stockfish"),
+        Tag(_.TimeControl, "300"),
+        Tag(_.ECO, "A00e")
+      )
+    ),
+    turns = List(
+      Turn(
+        number = 1,
+        white = Move("a4", secondsLeft = 298.some).some,
+        black = Move("Nf6", secondsLeft = 299.some).some
+      ),
+      Turn(
+        number = 2,
+        white = Move("d4", secondsLeft = 295.some).some,
+        black = Move("d5", secondsLeft = 298.some).some
+      ),
+      Turn(
+        number = 3,
+        white = Move("h4", secondsLeft = 292.some).some,
+        black = Move("e6", secondsLeft = 297.some).some
+      ),
+      Turn(
+        number = 4,
+        white = Move(
+          "Qd3",
+          glyphs = glyphs(1),
+          secondsLeft = 288.some,
+          comments = "An invention of true genius." :: Nil
+        ).some,
+        black = Move("c5", secondsLeft = 296.some).some
+      ),
+      Turn(
+        number = 5,
+        white = Move("dxc5", secondsLeft = 258.some).some,
+        black = Move("Bxc5", glyphs = glyphs(1), secondsLeft = 295.some).some
+      )
+    )
+  )
+
+  val pgn3 = Pgn(
+    tags = Tags.empty,
+    turns = List(
+      Turn(
+        number = 1,
+        white = Move("d3", glyphs = glyphs(6)).some,
+        black = Move("Nc6", glyphs = glyphs(10)).some
+      ),
+      Turn(
+        number = 2,
+        white = Move("Qd2").some,
+        black = Move(
+          "Nb4",
+          glyphs = Glyphs(
+            Glyph.MoveAssessment.blunder.some,
+            Glyph.PositionAssessment.whiteMuchBetter.some,
+            List(Glyph.Observation.timeTrouble)
+          )
+        ).some
+      ),
+      Turn(
+        number = 3,
+        white = Move("Qxb4", glyphs = glyphs(7)).some,
+        black = None
+      )
+    )
+  )
+
+  val pgn4 = Pgn(
+    tags = Tags.empty,
+    turns = List(
+      Turn(
+        number = 1,
+        white = Move(
+          "d4",
+          variations = List(
+            List(
+              Turn(
+                number = 1,
+                white = Move("e4").some,
+                black = None
+              )
+            )
+          )
+        ).some,
+        black = Move(
+          "Nf6",
+          variations = List(
+            List(
+              Turn(
+                number = 1,
+                white = None,
+                black = Move("d5").some
+              )
+            )
+          )
+        ).some
+      )
+    )
+  )
+  val pgn5 = Pgn(
+    tags = Tags(
+      List(
+        Tag(_.White, "tsinnema"),
+        Tag(_.Black, "[=0040.34h5a4]"),
+        Tag(_.TimeControl, "300"),
+        Tag(_.ECO, "A00e")
+      )
+    ),
+    turns = List(
+      Turn(
+        number = 1,
+        white = Move("a4", secondsLeft = 298.some).some,
+        black = Move("Nf6", secondsLeft = 299.some).some
+      ),
+      Turn(
+        number = 2,
+        white = Move("d4", secondsLeft = 295.some).some,
+        black = Move("d5", secondsLeft = 298.some).some
+      ),
+      Turn(
+        number = 3,
+        white = Move("h4", secondsLeft = 292.some).some,
+        black = Move("e6", secondsLeft = 297.some).some
+      ),
+      Turn(
+        number = 4,
+        white = Move(
+          "Qd3",
+          glyphs = glyphs(1),
+          secondsLeft = 288.some,
+          comments = "An invention of true genius." :: Nil
+        ).some,
+        black = Move("c5", secondsLeft = 296.some).some
+      ),
+      Turn(
+        number = 5,
+        white = Move("dxc5", secondsLeft = 258.some).some,
+        black = Move("Bxc5", glyphs = glyphs(1), secondsLeft = 295.some).some
+      )
+    )
+  )
+
+  val pgn6 = Pgn(
+    tags = Tags(
+      List(
+        Tag(_.Result, "0-1")
+      )
+    ),
+    turns = List()
+  )
+
+  val pgn7 = Pgn(
+    tags = Tags.empty,
+    turns = List()
+  )
+
+  val pgn8 = Pgn(
+    tags = Tags.empty,
+    turns = List(),
+    initial = Initial(List("Why hello there!"))
+  )
+
+  val pgn9 = Pgn(
+    tags = Tags.empty,
+    turns = List(),
+    initial = Initial(
+      List(
+        "Why hello there!",
+        "The Exchange Slav, the sure way to play with zero losing chances so an ideal choice for game one"
+      )
+    )
+  )
+
+  val pgn10 = Pgn(
+    tags = Tags.empty,
+    turns = List(
+      Turn(
+        number = 1,
+        white = Move(
+          "d4",
+          variations = List(
+            List(
+              Turn(
+                number = 1,
+                white = Move("e4").some,
+                black = None
+              )
+            )
+          )
+        ).some,
+        black = Move(
+          "Nf6",
+          variations = List(
+            List(
+              Turn(
+                number = 1,
+                white = None,
+                black = Move("d5").some
+              )
+            )
+          )
+        ).some
+      )
+    ),
+    initial = Initial(
+      List(
+        "Why hello there!",
+        "The Exchange Slav, the sure way to play with zero losing chances so an ideal choice for game one"
+      )
+    )
+  )
+
+  val pgn11 = Pgn(
+    tags = Tags(
+      List(
+        Tag(_.TimeControl, "\"")
+      )
+    ),
+    turns = List()
+  )
+
+  val pgn12 = Pgn(
+    tags = Tags(
+      List(
+        Tag(_.TimeControl, "\\")
+      )
+    ),
+    turns = List()
+  )
+
+  val pgns = List(pgn1, pgn2, pgn3, pgn4, pgn5, pgn6, pgn7, pgn8, pgn9, pgn10, pgn11, pgn12)
+
+  test("NewPgn and Pgn are isomorphic"):
+    pgns.foreach { pgn =>
+      val newPgn = NewPgn.toNewPgn(pgn)
+      assertEquals(pgn, newPgn.toPgn)
+    }

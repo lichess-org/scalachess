153e5308f6f64e5d4ccf7a2550353ef92c24d7d4
Thanh Le <lenguyenthanh@hotmail.com>
2023-06-08 15:40:04 +0200

    Add drop test for UciPathTest

--- src/test/scala/format/UciPathTest.scala
+++ src/test/scala/format/UciPathTest.scala
@@ -1,26 +1,35 @@
-package chess.format
+package chess
+package format
 
-import org.specs2.mutable.*
 import chess.Square
 
-class UciPathTest extends Specification:
+class UciPathTest extends munit.FunSuite:
 
-  "UciCharPair" should:
-    "format" in:
-      UciCharPair(Uci.Move(Square.E2, Square.E4)).toString === "/?"
+  test("empty intersect"):
+    assertEquals(UciPath("/?UE)8\\M(DYQDMTM'*`Y('aR-5").intersect(UciPath(")8VN")), UciPath.root)
 
-  "intersect" should:
-    "nope" in:
-      UciPath("/?UE)8\\M(DYQDMTM'*`Y('aR-5").intersect(UciPath(")8VN")) === UciPath.root
-    "full" in:
-      val p = UciPath("/?UE)8\\M(DYQDMTM'*`Y('aR-5")
-      p.intersect(p) === p
-    "partial left" in:
-      UciPath("/?UE)8\\M(DYQDMTM'*`Y('aR-5").intersect(UciPath("/?UE)8")) === UciPath("/?UE)8")
-    "partial right" in:
-      UciPath("/?UE)8").intersect(UciPath("/?UE)8\\M(DYQDMTM'*`Y('aR-5")) === UciPath("/?UE)8")
-  "utils" in:
+  test("full intersect"):
+    val p = UciPath("/?UE)8\\M(DYQDMTM'*`Y('aR-5")
+    assertEquals(p.intersect(p), p)
+
+  test("partial left"):
+    assertEquals(UciPath("/?UE)8\\M(DYQDMTM'*`Y('aR-5").intersect(UciPath("/?UE)8")), UciPath("/?UE)8"))
+
+  test("partial right"):
+    assertEquals(UciPath("/?UE)8").intersect(UciPath("/?UE)8\\M(DYQDMTM'*`Y('aR-5")), UciPath("/?UE)8"))
+
+  test("depth"):
     val p = UciPath(
       """.>VF-=F=/?WG)8`<%.<.&.G>8>aP$5^W'#_b08UE>-\M(=]O=OXO.N[^NWMW&^`^*&^&5&aX,<E<-<OG&7XO%-PV-5VE5=E;<6;L=DLV6EVEDEYI19"""
     )
-    p.depth === 57
+    assertEquals(p.depth, 57)
+
+  test("convert move to pair"):
+    assertEquals(UciCharPair(Uci.Move(Square.E2, Square.E4)).toString, "/?")
+
+  test("convert drop to pair"):
+    assertEquals(UciCharPair(Uci.Drop(Pawn, Square.C7)), UciCharPair('U', '\u008f'))
+    assertEquals(UciCharPair(Uci.Drop(Knight, Square.C7)), UciCharPair('U', '\u008e'))
+    assertEquals(UciCharPair(Uci.Drop(Bishop, Square.C7)), UciCharPair('U', '\u008d'))
+    assertEquals(UciCharPair(Uci.Drop(Rook, Square.C7)), UciCharPair('U', '\u008c'))
+    assertEquals(UciCharPair(Uci.Drop(Queen, Square.C7)), UciCharPair('U', '\u008b'))

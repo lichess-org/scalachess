bf262a992b2925240bac949e079448ab4c1eeeee
Thanh Le <lenguyenthanh@hotmail.com>
2023-06-13 09:50:25 +0200

    Remove superfluous using

--- src/main/scala/HasId.scala
+++ src/main/scala/HasId.scala
@@ -2,10 +2,10 @@ package chess
 
 trait HasId[A, Id]:
   def getId(a: A): Id
-  extension [A](a: A)
-    inline def id[Id](using hasId: HasId[A, Id]): Id             = hasId.getId(a)
-    inline def sameId[Id](other: A)(using HasId[A, Id]): Boolean = a.id == other.id
-    inline def hasId[Id](id: Id)(using HasId[A, Id]): Boolean    = a.id == id
+  extension (a: A)
+    inline def id: Id                    = getId(a)
+    inline def sameId(other: A): Boolean = a.id == other.id
+    inline def hasId(id: Id): Boolean    = a.id == id
 
 // trait for merge two values of the same type
 // A may not sastify Semigroup laws, hence new Mergeable Trait

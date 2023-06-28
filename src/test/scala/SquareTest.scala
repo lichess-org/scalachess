4c20d4d5b4c6848715d0b8a5a4f1cfd2c0749a73
Thanh Le <lenguyenthanh@hotmail.com>
2023-06-28 23:41:44 +0200

    Add some more tests for Square

--- src/test/scala/SquareTest.scala
+++ src/test/scala/SquareTest.scala
@@ -1,13 +1,17 @@
 package chess
 
+import cats.syntax.all.*
 import Square.*
+import munit.ScalaCheckSuite
+import Arbitraries.given
+import org.scalacheck.Prop.forAll
 
-class SquareTest extends munit.FunSuite:
+class SquareTest extends ScalaCheckSuite:
 
   test("keys"):
     assertEquals(D5.key, "d5")
 
-  test("chars"):
+  test("chars for some squares"):
     assertEquals(A1.asChar, 'a')
     assertEquals(B4.asChar, 'z')
     assertEquals(C4.asChar, 'A')
@@ -17,3 +21,29 @@ class SquareTest extends munit.FunSuite:
     assertEquals(F8.asChar, '9')
     assertEquals(G8.asChar, '!')
     assertEquals(H8.asChar, '?')
+
+  test("chars.fromChar == some.identity"):
+    Square.all.foreach: square =>
+      assertEquals(Square.fromChar(square.asChar), square.some)
+
+  test("chars are unique"):
+    Square.all.map(_.asChar).toSet.size == 64
+
+  test("keys.fromKey == some.identity"):
+    Square.all.foreach: square =>
+      assertEquals(Square.fromKey(square.key), square.some)
+
+  test("keys are unique"):
+    Square.all.map(_.key).toSet.size == 64
+
+  test("x onSame x == true"):
+    Square.all.foreach: square =>
+      assert(square onSameLine square)
+      assert(square onSameRank square)
+      assert(square onSameDiagonal square)
+
+  test("x onSame y == y onSame x"):
+    forAll: (x: Square, y: Square) =>
+      assertEquals(x onSameLine y, y onSameLine x)
+      assertEquals(x onSameRank y, y onSameRank x)
+      assertEquals(x onSameDiagonal y, y onSameDiagonal x)

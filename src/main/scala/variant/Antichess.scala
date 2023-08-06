c605183c69f3044b5fe1f8d9c74b0224fa370b47
oziomaogbe <ogbeoziomajnr@yahoo.com>
2023-08-06 19:23:24 +0700

    Fix formatting.

--- src/main/scala/variant/Antichess.scala
+++ src/main/scala/variant/Antichess.scala
@@ -37,7 +37,7 @@ case object Antichess
 
   override def valid(situation: Situation, strict: Boolean) =
     situation.board.nbPieces >= 2 && situation.board.nbPieces <= 32
-  
+
   // In antichess, there is no checkmate condition, and the winner is the current player if they have no legal moves
   override def winner(situation: Situation): Option[Color] =
     specialEnd(situation) option situation.color

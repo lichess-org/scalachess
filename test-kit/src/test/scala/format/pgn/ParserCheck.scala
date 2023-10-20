8dacf3476af178b41c685c0d8c3ca1fde375d7dd
Thanh Le <lenguyenthanh@hotmail.com>
2023-10-20 17:22:15 +0200

    Fix test compiling

--- test-kit/src/test/scala/format/pgn/ParserCheck.scala
+++ test-kit/src/test/scala/format/pgn/ParserCheck.scala
@@ -13,5 +13,5 @@ class ParserCheck extends ScalaCheckSuite:
   test("ParserCheck"):
     forAll(genPgn(Situation(Standard))): pgn =>
       val str    = pgn.render
-      val result = Parser.full(str).get.toPgn.render
+      val result = Parser.full(str).toOption.get.toPgn.render
       assertEquals(result, str)

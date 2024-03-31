54dedf8e8112c9479b74884a47c53b52de0f085f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-03-31 22:59:27 +0200

    binaryfen: factor out benchmark fixtures

--- bench/src/main/scala/benchmarks/BinaryFenBench.scala
+++ bench/src/main/scala/benchmarks/BinaryFenBench.scala
@@ -21,43 +21,43 @@ class BinaryFenBench:
   // the unit of CPU work per iteration
   private val Work: Long = 10
 
+  private val binary = BinaryFen(
+    Array(
+      0xff.toByte,
+      0xff.toByte,
+      0x00.toByte,
+      0x00.toByte,
+      0x10.toByte,
+      0x00.toByte,
+      0xef.toByte,
+      0xff.toByte,
+      0x2d.toByte,
+      0x84.toByte,
+      0x4a.toByte,
+      0xd2.toByte,
+      0x00.toByte,
+      0x00.toByte,
+      0x00.toByte,
+      0x00.toByte,
+      0x11.toByte,
+      0x11.toByte,
+      0x11.toByte,
+      0x11.toByte,
+      0x3e.toByte,
+      0x95.toByte,
+      0x5f.toByte,
+      0xe3.toByte
+    )
+  )
+
   @Benchmark
   def read(bh: Blackhole) =
     Blackhole.consumeCPU(Work)
-    bh.consume(
-      BinaryFen.read(
-        BinaryFen(
-          Array(
-            0xff.toByte,
-            0xff.toByte,
-            0x00.toByte,
-            0x00.toByte,
-            0x10.toByte,
-            0x00.toByte,
-            0xef.toByte,
-            0xff.toByte,
-            0x2d.toByte,
-            0x84.toByte,
-            0x4a.toByte,
-            0xd2.toByte,
-            0x00.toByte,
-            0x00.toByte,
-            0x00.toByte,
-            0x00.toByte,
-            0x11.toByte,
-            0x11.toByte,
-            0x11.toByte,
-            0x11.toByte,
-            0x3e.toByte,
-            0x95.toByte,
-            0x5f.toByte,
-            0xe3.toByte
-          )
-        )
-      )
-    )
+    bh.consume(BinaryFen.read(binary))
+
+  private val situation = Situation.AndFullMoveNumber(Situation(Standard), FullMoveNumber(1))
 
   @Benchmark
   def write(bh: Blackhole) =
     Blackhole.consumeCPU(Work)
-    bh.consume(BinaryFen.write(Situation.AndFullMoveNumber(Situation(Standard), FullMoveNumber(1))))
+    bh.consume(BinaryFen.write(situation))

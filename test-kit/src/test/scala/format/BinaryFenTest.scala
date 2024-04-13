b557cd2c4c52fde7b05148bb62b2c299042cb22f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-04-13 15:30:17 +0200

    also check hashCode in equality test

--- test-kit/src/test/scala/format/BinaryFenTest.scala
+++ test-kit/src/test/scala/format/BinaryFenTest.scala
@@ -35,10 +35,12 @@ class BinaryFenTest extends ScalaCheckSuite:
       val rewritten = BinaryFen.write(BinaryFen(bytes).read)
       assertEquals(BinaryFen.write(rewritten.read), rewritten)
 
-  test("equals are sensiable"):
+  test("equals is sensible"):
     forAll: (bytes: Array[Byte]) =>
       val anotherByes = bytes.clone()
+      assertEquals(BinaryFen(bytes).hashCode, BinaryFen(anotherByes).hashCode)
       assertEquals(BinaryFen(bytes), BinaryFen(anotherByes))
+
   test("handpicked fens roundtrip"):
     assertRoundtrip(Standard, FullFen("8/8/8/8/8/8/8/8 w - - 0 1"))
     assertRoundtrip(Standard, FullFen("8/8/8/8/8/8/8/8 b - - 0 1"))

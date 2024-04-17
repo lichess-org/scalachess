4a0929ce04d7f8e5d9f6b373577eb559259891d0
Thibault Duplessis <t@lichess.org>
2024-04-17 08:48:08 +0200

    add CorrespondenceClock

---
+++ core/src/main/scala/CorrespondenceClock.scala
@@ -0,0 +1,44 @@
+package chess
+
+import java.time.Instant
+import scalalib.time.{ toSeconds, nowSeconds }
+
+// times are expressed in seconds
+case class CorrespondenceClock(
+    increment: Int,
+    whiteTime: Float,
+    blackTime: Float
+):
+  import CorrespondenceClock.*
+
+  def daysPerTurn: Int = increment / 60 / 60 / 24
+
+  def remainingTime(c: Color): Float = c.fold(whiteTime, blackTime)
+
+  def outoftime(c: Color): Boolean = remainingTime(c) == 0
+
+  def moretimeable(c: Color): Boolean = remainingTime(c) < (increment - hourSeconds)
+
+  def giveTime(c: Color): CorrespondenceClock =
+    c.fold(
+      copy(whiteTime = whiteTime + daySeconds),
+      copy(blackTime = blackTime + daySeconds)
+    )
+
+  // in seconds
+  def estimateTotalTime: Int = increment * 40 / 2
+
+  def incrementHours: Int = increment / 60 / 60
+
+object CorrespondenceClock:
+  val hourSeconds = 60 * 60
+  val daySeconds  = 24 * hourSeconds
+
+  def apply(daysPerTurn: Int, turnColor: Color, lastMoveAt: Instant): CorrespondenceClock =
+    val increment   = daysPerTurn * 24 * 60 * 60
+    val secondsLeft = (lastMoveAt.toSeconds + increment - nowSeconds).toInt.max(0)
+    CorrespondenceClock(
+      increment = increment,
+      whiteTime = turnColor.fold(secondsLeft, increment).toFloat,
+      blackTime = turnColor.fold(increment, secondsLeft).toFloat
+    )

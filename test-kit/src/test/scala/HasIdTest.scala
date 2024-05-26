12339917088597180753d0bb2024b7f987f9a837
Thanh Le <lenguyenthanh@hotmail.com>
2024-05-26 10:27:21 +0700

    Add tests for HasId.removeById

--- test-kit/src/test/scala/HasIdTest.scala
+++ test-kit/src/test/scala/HasIdTest.scala
@@ -1,20 +1,29 @@
 package chess
 
 import munit.ScalaCheckSuite
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.{ forAll, propBoolean }
 
 class HasIdTest extends ScalaCheckSuite:
 
   given HasId[Int, Int] with
     extension (a: Int) def id: Int = a
 
-  test("removeById.size <= size"):
+  test("if there is no id, removeById does nothing"):
     forAll: (xs: List[Int], id: Int) =>
-      val removed = xs.removeById(id)
-      xs.size == removed.size || xs.size == removed.size + 1
+      xs.indexOf(id) == -1 ==> (xs.removeById(id) == xs)
 
   test("removeById.size <= size"):
     forAll: (xs: List[Int], id: Int) =>
       val removed = xs.removeById(id)
       val epsilon = if xs.find(_ == id).isDefined then 1 else 0
       xs.size == removed.size + epsilon
+
+  test("removeById reserves order"):
+    forAll: (xs: List[Int], id: Int) =>
+      val sorted  = xs.sorted
+      val removed = sorted.removeById(id)
+      removed == removed.sorted
+
+  test("removeById only remove first items"):
+    val xs = List(1, 1)
+    xs.removeById(1) == List(1)

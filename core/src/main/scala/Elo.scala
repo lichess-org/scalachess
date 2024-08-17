2662f4614c74e08719bc919c575283a81b8e7a8f
Thibault Duplessis <t@lichess.org>
2024-08-17 11:08:07 +0200

    Elo calculation tweaks

--- core/src/main/scala/Elo.scala
+++ core/src/main/scala/Elo.scala
@@ -6,18 +6,30 @@ opaque type KFactor = Int
 object KFactor extends OpaqueInt[KFactor]:
   val default = KFactor(40)
 
+/*
+ * https://handbook.fide.com/chapter/B022022
+ * https://ratings.fide.com/calc.phtml
+ * */
 object Elo extends OpaqueInt[Elo]:
 
   final class Player(val rating: Elo, val kFactor: KFactor)
   final class Game(val win: Option[Boolean], val opponentRating: Elo)
 
+  /* 8.3.1
+   * For each game played against a rated player, determine the difference in rating between the player and their opponent.
+   * A difference in rating of more than 400 points shall be counted for rating purposes as though it were a difference of 400 points.  In any tournament, a player may benefit from only one upgrade under this rule, for the game in which the rating difference is greatest. */
+  def playersRatingDiff(a: Elo, b: Elo): Int =
+    Math.min(400, Math.max(-400, b - a))
+
   // https://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details
   def computeNewRating(player: Player, games: Seq[Game]): Elo =
     val expectedScore = games.foldLeft(0d): (score, game) =>
-      score + 1 / (1 + Math.pow(10, (game.opponentRating - player.rating).value / 400))
+      val prd = playersRatingDiff(player.rating, game.opponentRating)
+      score + 1 / (1 + Math.pow(10, prd / 400d))
     val achievedScore = games.foldLeft(0d): (score, game) =>
       score + game.win.fold(0.5d)(if _ then 1d else 0d)
-    val ratingDiff = Math.round(player.kFactor * (achievedScore - expectedScore)).toInt
+    val ratingDiff =
+      Math.round(player.kFactor * (achievedScore - expectedScore)).toInt
     player.rating + ratingDiff
 
   def computeRatingDiff(player: Player, games: Seq[Game]): Int =
--- test-kit/src/test/scala/EloTest.scala
+++ test-kit/src/test/scala/EloTest.scala
@@ -2,24 +2,23 @@ package chess
 
 class EloTest extends ChessTest:
 
-  private def newRating(r: Int, k: Int, winOpt: Option[Boolean], opRating: Int): Int =
+  private def ratingDiff(r: Int, k: Int, opRating: Int, winOpt: Option[Boolean], expected: Int)(using
+      munit.Location
+  ) =
     val player = Elo.Player(Elo(r), KFactor(k))
     val game   = Elo.Game(winOpt, Elo(opRating))
-    Elo.computeNewRating(player, List(game)).value
+    assertEquals(Elo.computeRatingDiff(player, List(game)), expected)
 
-  test("new rating calculation"):
-    List(
-      (1500, 40, 1500, Some(true), 1520),
-      (1500, 40, 1500, Some(false), 1480),
-      (1500, 40, 1500, None, 1500),
-      (1500, 40, 1900, Some(true), 1536),
-      (1500, 40, 1900, Some(false), 1496),
-      (1500, 40, 1900, None, 1516),
-      (1500, 40, 2000, Some(true), 1536),
-      (1500, 40, 2000, Some(false), 1496),
-      (1500, 40, 2000, None, 1516),
-      (1500, 40, 1600, Some(true), 1520),
-      (1500, 40, 1600, Some(false), 1485),
-      (1500, 40, 1600, None, 1505)
-    ).foreach: (rating, k, opRating, win, expected) =>
-      assertEquals(newRating(rating, k, win, opRating), expected)
+  test("new rating calculation over one game"):
+    ratingDiff(1500, 40, 1500, Some(true), 20)
+    ratingDiff(1500, 40, 1500, Some(false), -20)
+    ratingDiff(1500, 40, 1500, None, 0)
+    ratingDiff(1500, 40, 1900, Some(true), 36)
+    ratingDiff(1500, 40, 1900, Some(false), -4)
+    ratingDiff(1500, 40, 1900, None, 16)
+    ratingDiff(1500, 40, 2900, Some(true), 36)
+    ratingDiff(1500, 40, 2900, Some(false), -4)
+    ratingDiff(1500, 40, 2900, None, 16)
+    ratingDiff(1500, 40, 1600, Some(true), 26)
+    ratingDiff(1500, 40, 1600, Some(false), -14)
+    ratingDiff(1500, 40, 1600, None, 6)

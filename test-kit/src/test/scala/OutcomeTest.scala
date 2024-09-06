18edf46a50445048fdc2ee5a83752e5b3884f490
Thibault Duplessis <t@lichess.org>
2024-09-06 19:23:23 +0200

    more outcome tests

--- test-kit/src/test/scala/OutcomeTest.scala
+++ test-kit/src/test/scala/OutcomeTest.scala
@@ -10,12 +10,15 @@ class OutcomeTest extends ScalaCheckSuite:
     assertEquals(fromResult("1-0"), Some(white))
     assertEquals(fromResult("0-1"), Some(black))
     assertEquals(fromResult("1/2-1/2"), Some(draw))
+    assertEquals(fromResult(""), None)
+    assertEquals(fromResult("*"), None)
 
   test("silly format outcomes"):
     assertEquals(fromResult("+_-"), Some(white))
     assertEquals(fromResult("0:1"), Some(black))
     assertEquals(fromResult("½-½"), Some(draw))
     assertEquals(fromResult("WHITEWIN"), Some(white))
+    assertEquals(fromResult("DRAW"), Some(draw))
 
   test("points"):
     def normalize(s: String) = showPoints(pointsFromResult(s))
@@ -24,3 +27,4 @@ class OutcomeTest extends ScalaCheckSuite:
     assertEquals(normalize("—:—"), "0-0")
     assertEquals(normalize("1/2_0"), "1/2-0")
     assertEquals(normalize("0-½"), "0-1/2")
+    assertEquals(normalize("*"), "*")

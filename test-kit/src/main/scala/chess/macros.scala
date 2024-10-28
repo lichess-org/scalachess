634af22bada520687d095438bfc7f1e609709849
Thanh Le <lenguyenthanh@hotmail.com>
2024-10-28 09:01:33 +0100

    Add literal uci

--- test-kit/src/main/scala/chess/macros.scala
+++ test-kit/src/main/scala/chess/macros.scala
@@ -1,16 +1,27 @@
 package chess
 
 import cats.syntax.all.*
-import org.typelevel.literally.Literally
+import chess.format.Uci
 import chess.format.pgn.*
+import org.typelevel.literally.Literally
 
 object macros:
   extension (inline ctx: StringContext)
+
     inline def pgn(inline args: Any*): ParsedPgn =
       ${ PgnLiteral('ctx, 'args) }
+
+    inline def uci(inline args: Any*): Uci =
+      ${ UciLiteral('ctx, 'args) }
 
   object PgnLiteral extends Literally[ParsedPgn]:
     def validate(s: String)(using Quotes) =
       Parser.full(PgnStr(s)) match
         case Right(parsed) => Right('{ Parser.full(PgnStr(${ Expr(s) })).toOption.get })
         case Left(err)     => Left(err.toString)
+
+  object UciLiteral extends Literally[Uci]:
+    def validate(s: String)(using Quotes) =
+      Uci(s) match
+        case Some(_) => Right('{ Uci(${ Expr(s) }).get })
+        case _       => Left(s"Invalid UCI: $s")

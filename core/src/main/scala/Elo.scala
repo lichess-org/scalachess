beb17b55636f1dc31b503bca0546ebc6c413fe09
Arkadiusz Klonowski <arkadiusz.klonowski.1995@gmail.com>
2024-11-11 02:31:49 +0100

    implement FIDE table for rating performance calculation

--- core/src/main/scala/Elo.scala
+++ core/src/main/scala/Elo.scala
@@ -38,15 +38,10 @@ object Elo extends RelaxedOpaqueInt[Elo]:
     if ratingDiff <= 0 then expectedScore else 1.0f - expectedScore
 
   def computePerformanceRating(games: Seq[Game]): Option[Elo] =
-    val winBonus = 400
     games.nonEmpty.option:
-      val ratings = games.map(_.opponentRating).sum
-      val points = games.foldMap:
-        _.points.match
-          case Outcome.Points.Zero => -1
-          case Outcome.Points.Half => 0
-          case Outcome.Points.One  => 1
-      (ratings + points * winBonus) / games.size
+      val averageOpponentRating = games.map(_.opponentRating).sum / games.size
+      val percentageScore = BigDecimal(games.map(_.points.value).sum / games.size).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+      averageOpponentRating + performanceRatingTableFIDE.getOrElse(percentageScore, 0)
 
   final class Player(val rating: Elo, val kFactor: KFactor)
 
@@ -105,3 +100,111 @@ object Elo extends RelaxedOpaqueInt[Elo]:
   // the hardcoded List above is not really necessary,
   // but it mirrors the reference table on
   // https://handbook.fide.com/chapter/B022022 8.1.2
+
+
+  // 1.4.9 FIDE table
+  val performanceRatingTableFIDE: Map[BigDecimal, Int] = Map(
+    BigDecimal("0.00") -> -800,
+    BigDecimal("0.01") -> -677,
+    BigDecimal("0.02") -> -589,
+    BigDecimal("0.03") -> -538,
+    BigDecimal("0.04") -> -501,
+    BigDecimal("0.05") -> -470,
+    BigDecimal("0.06") -> -444,
+    BigDecimal("0.07") -> -422,
+    BigDecimal("0.08") -> -401,
+    BigDecimal("0.09") -> -383,
+    BigDecimal("0.10") -> -366,
+    BigDecimal("0.11") -> -351,
+    BigDecimal("0.12") -> -336,
+    BigDecimal("0.13") -> -322,
+    BigDecimal("0.14") -> -309,
+    BigDecimal("0.15") -> -296,
+    BigDecimal("0.16") -> -284,
+    BigDecimal("0.17") -> -273,
+    BigDecimal("0.18") -> -262,
+    BigDecimal("0.19") -> -251,
+    BigDecimal("0.20") -> -240,
+    BigDecimal("0.21") -> -230,
+    BigDecimal("0.22") -> -220,
+    BigDecimal("0.23") -> -211,
+    BigDecimal("0.24") -> -202,
+    BigDecimal("0.25") -> -193,
+    BigDecimal("0.26") -> -184,
+    BigDecimal("0.27") -> -175,
+    BigDecimal("0.28") -> -166,
+    BigDecimal("0.29") -> -158,
+    BigDecimal("0.30") -> -149,
+    BigDecimal("0.31") -> -141,
+    BigDecimal("0.32") -> -133,
+    BigDecimal("0.33") -> -125,
+    BigDecimal("0.34") -> -117,
+    BigDecimal("0.35") -> -110,
+    BigDecimal("0.36") -> -102,
+    BigDecimal("0.37") -> -95,
+    BigDecimal("0.38") -> -87,
+    BigDecimal("0.39") -> -80,
+    BigDecimal("0.40") -> -72,
+    BigDecimal("0.41") -> -65,
+    BigDecimal("0.42") -> -57,
+    BigDecimal("0.43") -> -50,
+    BigDecimal("0.44") -> -43,
+    BigDecimal("0.45") -> -36,
+    BigDecimal("0.46") -> -29,
+    BigDecimal("0.47") -> -21,
+    BigDecimal("0.48") -> -14,
+    BigDecimal("0.49") -> -7,
+    BigDecimal("0.50") -> 0,
+    BigDecimal("0.51") -> 7,
+    BigDecimal("0.52") -> 14,
+    BigDecimal("0.53") -> 21,
+    BigDecimal("0.54") -> 29,
+    BigDecimal("0.55") -> 36,
+    BigDecimal("0.56") -> 43,
+    BigDecimal("0.57") -> 50,
+    BigDecimal("0.58") -> 57,
+    BigDecimal("0.59") -> 65,
+    BigDecimal("0.60") -> 72,
+    BigDecimal("0.61") -> 80,
+    BigDecimal("0.62") -> 87,
+    BigDecimal("0.63") -> 95,
+    BigDecimal("0.64") -> 102,
+    BigDecimal("0.65") -> 110,
+    BigDecimal("0.66") -> 117,
+    BigDecimal("0.67") -> 125,
+    BigDecimal("0.68") -> 133,
+    BigDecimal("0.69") -> 141,
+    BigDecimal("0.70") -> 149,
+    BigDecimal("0.71") -> 158,
+    BigDecimal("0.72") -> 166,
+    BigDecimal("0.73") -> 175,
+    BigDecimal("0.74") -> 184,
+    BigDecimal("0.75") -> 193,
+    BigDecimal("0.76") -> 202,
+    BigDecimal("0.77") -> 211,
+    BigDecimal("0.78") -> 220,
+    BigDecimal("0.79") -> 230,
+    BigDecimal("0.80") -> 240,
+    BigDecimal("0.81") -> 251,
+    BigDecimal("0.82") -> 262,
+    BigDecimal("0.83") -> 273,
+    BigDecimal("0.84") -> 284,
+    BigDecimal("0.85") -> 296,
+    BigDecimal("0.86") -> 309,
+    BigDecimal("0.87") -> 322,
+    BigDecimal("0.88") -> 336,
+    BigDecimal("0.89") -> 351,
+    BigDecimal("0.90") -> 366,
+    BigDecimal("0.91") -> 383,
+    BigDecimal("0.92") -> 401,
+    BigDecimal("0.93") -> 422,
+    BigDecimal("0.94") -> 444,
+    BigDecimal("0.95") -> 470,
+    BigDecimal("0.96") -> 501,
+    BigDecimal("0.97") -> 538,
+    BigDecimal("0.98") -> 589,
+    BigDecimal("0.99") -> 677,
+    BigDecimal("1.00") -> 800
+  )
+
+

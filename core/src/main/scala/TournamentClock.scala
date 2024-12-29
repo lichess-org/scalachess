293a05cf3946a5b3324f24f06d02415d1642b1a2
Thibault Duplessis <t@lichess.org>
2024-12-29 10:51:07 +0100

    TournamentClock.parse with tests passing

--- core/src/main/scala/TournamentClock.scala
+++ core/src/main/scala/TournamentClock.scala
@@ -1,9 +1,25 @@
 package chess
 
+import cats.syntax.all.*
 import Clock.{ LimitSeconds, IncrementSeconds }
 
 case class TournamentClock(limitSeconds: LimitSeconds, incrementSeconds: IncrementSeconds)
 
 object TournamentClock:
 
-  def parse(str: String): Option[TournamentClock] = None
+  object parse:
+
+    private val cleanRegex = "(move|minutes|minute|min|m|seconds|second|sec|s|'|\"|/)".r
+
+    private def make(a: Int, b: Int) =
+      val limit = LimitSeconds(if a > 180 then a else a * 60)
+      TournamentClock(limit, IncrementSeconds(b))
+
+    def apply(str: String): Option[TournamentClock] =
+      val clean = cleanRegex.replaceAllIn(str.toLowerCase, "").replace(" ", "")
+      clean.toIntOption
+        .map(make(_, 0))
+        .orElse:
+          clean.split('+') match
+            case Array(a, b) => (a.toIntOption, b.toIntOption).mapN(make)
+            case _           => none

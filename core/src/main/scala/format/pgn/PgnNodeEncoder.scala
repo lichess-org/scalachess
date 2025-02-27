b8e4361e83c5b903355f074b63a863dfbfc1572c
GraveNews <87426252+gravenewsweekly@users.noreply.github.com>
2025-02-27 11:26:44 +0530

    Update PgnNodeEncoder.scala

--- core/src/main/scala/format/pgn/PgnNodeEncoder.scala
+++ core/src/main/scala/format/pgn/PgnNodeEncoder.scala
@@ -5,7 +5,7 @@ import cats.syntax.all.*
 
 /**
  * PgnNodeEncoder,
- * Provide encoding of a node to a string, which is used to render a PGN string
+ * Provides encoding of a node to a string, which is used to render a PGN string
  * from a chess.Tree[A]
  */
 trait PgnNodeEncoder[A]:
@@ -23,33 +23,34 @@ object PgnNodeEncoder:
         startPly: Ply
     ): PgnNodeEncoder[B] ?=> Option[PgnStr] =
       tree
-        .mapAccumlOption_(context): (context, a) =>
+        .mapAccumlOption_(context) { (context, a) =>
           f(context, a).fold(context -> none)(_ -> _.some)
+        }
         .map(_.toPgnStr(startPly))
 
   extension [A](tree: Tree[A])
 
     /**
-     * render a tree to a PgnStr
+     * Render a tree to a PgnStr
      */
     def toPgnStr(startPly: Ply): PgnNodeEncoder[A] ?=> PgnStr =
-      PgnStr:
+      PgnStr {
         val builder = new StringBuilder
         appendPgnStr(builder, startPly)
         builder.toString
+      }
 
     /**
-     * append the rendred PgnStr to the builder
+     * Append the rendered PgnStr to the builder
      */
     def appendPgnStr(builder: StringBuilder, ply: Ply): PgnNodeEncoder[A] ?=> Unit =
-      render(builder, !ply.isWhiteTurn, ply)
+      render(builder, forceTurnNumber(ply), ply)
 
-    // We force to render turn number for the next black turn when the current value
-    // has comment(s) or variation(s) or the rendered string of this value is not compact
-    // so, this returns true if the current value is black
-    // or the current value is white and has comment(s) or variation(s)
+    /**
+     * Determines whether to force rendering the turn number
+     */
     private def forceTurnNumber(ply: Ply): PgnNodeEncoder[A] ?=> Boolean =
-      !ply.isWhiteTurn || (tree.value.hasComment || tree.variations.nonEmpty)
+      !ply.isWhiteTurn || tree.value.hasComment || tree.variations.nonEmpty
 
     @annotation.tailrec
     private def render(
@@ -58,32 +59,35 @@ object PgnNodeEncoder:
         ply: Ply
     ): PgnNodeEncoder[A] ?=> Unit =
       if tree.isVariation then tree.value.appendVariationComment(builder)
+
       tree.addTurnNumberPrefix(forceTurnNumber, builder, ply)
       renderValueAndVariations(builder, ply)
-      tree.child.match
-        case None => ()
-        case Some(x) =>
-          builder.addOne(' ')
-          x.render(builder, tree.forceTurnNumber(ply), ply.next)
 
-    // Add turn number prefix to the builder if needed
-    // if the current value is white, We ignore forceTurnNumber value as
-    // it always renders with a turn number and a dot for example: `1. e4`
-    // if the current value is black and forceTurnNumber is true it needs to
-    // render with a turn number and 3 dots for example: `1... e5`
+      tree.child match
+        case None    => ()
+        case Some(x) =>
+          builder.append(' ')
+          x.render(builder, x.forceTurnNumber(ply), ply.next)
+
+    /**
+     * Adds the turn number prefix to the builder if required
+     */
     private def addTurnNumberPrefix(forceTurnNumber: Boolean, builder: StringBuilder, ply: Ply): Unit =
       if ply.isWhiteTurn then builder.append(ply.turnNumber).append(". ")
       else if forceTurnNumber then builder.append(ply.turnNumber).append("... ")
 
+    /**
+     * Appends the move and variations to the builder
+     */
     private def renderValueAndVariations(builder: StringBuilder, ply: Ply): PgnNodeEncoder[A] ?=> Unit =
       tree.value.appendSanStr(builder)
-      tree.variations.foreach: x =>
-        builder.addOne(' ').addOne('(')
+      tree.variations.foreach { x =>
+        builder.append(' ').append('(')
         x.appendPgnStr(builder, ply)
-        builder.addOne(')')
+        builder.append(')')
+      }
 
   extension (ply: Ply)
-    private def isWhiteTurn: Boolean =
-      ply.isOdd
+    private def isWhiteTurn: Boolean = ply.isOdd
     private def turnNumber: FullMoveNumber =
-      ply.fullMoveNumber.map(_ + ply.value % 2 - 1)
+      ply.fullMoveNumber.map(_ + (if ply.isOdd then 0 else -1))

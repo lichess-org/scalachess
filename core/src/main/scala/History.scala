b1aa6b73188aeec058d5a70e74c15272b96bb2b1
Thanh Le <lenguyenthanh@hotmail.com>
2025-05-03 12:40:25 +0200

    Move CheckCount to after History
    and type annotations

--- core/src/main/scala/History.scala
+++ core/src/main/scala/History.scala
@@ -4,19 +4,6 @@ import chess.variant.Crazyhouse
 
 import format.Uci
 
-// Checks received by the respective side.
-case class CheckCount(white: Int = 0, black: Int = 0):
-
-  def add(color: Color) =
-    copy(
-      white = white + color.fold(1, 0),
-      black = black + color.fold(0, 1)
-    )
-
-  def nonEmpty = white > 0 || black > 0
-
-  def apply(color: Color) = color.fold(white, black)
-
 case class History(
     lastMove: Option[Uci] = None,
     positionHashes: PositionHash = PositionHash.empty,
@@ -49,3 +36,16 @@ case class History(
     if check.yes then copy(checkCount = checkCount.add(color)) else this
 
   def withCheckCount(cc: CheckCount): History = copy(checkCount = cc)
+
+// Checks received by the respective side.
+case class CheckCount(white: Int = 0, black: Int = 0):
+
+  def add(color: Color): CheckCount =
+    copy(
+      white = white + color.fold(1, 0),
+      black = black + color.fold(0, 1)
+    )
+
+  def apply(color: Color): Int = color.fold(white, black)
+
+  def nonEmpty: Boolean = white > 0 || black > 0

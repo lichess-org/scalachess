167b2c607097c1fd5f9d4716a99b20f6f757be47
Thibault Duplessis <t@lichess.org>
2025-09-29 11:14:24 +0200

    re-add legacy castling notation support
    for API clients

--- core/src/main/scala/format/UciDump.scala
+++ core/src/main/scala/format/UciDump.scala
@@ -9,18 +9,22 @@ object UciDump:
   def apply(
       moves: Seq[pgn.SanStr],
       initialFen: Option[FullFen],
-      variant: Variant
+      variant: Variant,
+      // some API clients can't handle e1h1, so we need to send them e1g1
+      legacyStandardCastling: Boolean = false
   ): Either[ErrorStr, List[String]] =
     if moves.isEmpty then Nil.asRight
     else
       Position(variant, initialFen)
         .play(moves, Ply.initial): step =>
-          move(step.move)
+          move(step.move, variant, legacyStandardCastling)
 
-  def move(mod: MoveOrDrop): String =
+  def move(mod: MoveOrDrop, variant: Variant, legacyStandardCastling: Boolean = false): String =
     mod match
       case m: Move =>
         m.castle
           .fold(m.toUci.uci): c =>
-            c.king.key + c.rook.key
+            if legacyStandardCastling && variant.standard
+            then c.king.key + c.kingTo.key
+            else c.king.key + c.rook.key
       case d: Drop => d.toUci.uci
